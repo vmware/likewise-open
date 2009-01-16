2fff80da6a9f23bf05ddbda53dda8382719e8227
Sriram Nambakam <snambakam@likewise.com>
2009-01-16 20:19:34 +0000

    move contents of srvstructs.h to structs.h delete srvstructs.h
    (lwio: r30791)

--- lwio/server/srv/structs.h
+++ lwio/server/srv/structs.h
@@ -72,6 +72,36 @@ typedef struct _SMB_SRV_SHARE_DB_CONTEXT
 
 } SMB_SRV_SHARE_DB_CONTEXT, *PSMB_SRV_SHARE_DB_CONTEXT;
 
+typedef struct _SRV_CCB
+{
+    //LIST_ENTRY NextCCB;
+    UNICODE_STRING AbsolutePathName;
+    ACCESS_MASK DesiredAccess;
+    LONG64 AllocationSize;
+    FILE_ATTRIBUTES FileAttributes;
+    FILE_SHARE_FLAGS ShareAccess;
+    FILE_CREATE_DISPOSITION CreateDisposition;
+    FILE_CREATE_OPTIONS CreateOptions;
+    PIO_EA_BUFFER pEaBuffer;
+
+    int fd;
+    char *path;
+    int oflags;
+    mode_t mode;
+
+} SRV_CCB, *PSRV_CCB;
+
+typedef struct _SRV_IRP_CONTEXT
+{
+    PIRP             pIrp;
+    IO_DEVICE_HANDLE targetDeviceHandle;
+    UNICODE_STRING   rootPathName;
+    UNICODE_STRING   relativePathName;
+    UNICODE_STRING   absolutePathName;
+
+} SRV_IRP_CONTEXT, *PSRV_IRP_CONTEXT;
+
+
 typedef VOID (*PFN_PROD_CONS_QUEUE_FREE_ITEM)(PVOID pItem);
 
 typedef struct _SMB_PROD_CONS_QUEUE
