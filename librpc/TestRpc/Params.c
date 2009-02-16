fa621d64f134e3994d6fc7f551c6e76bc2c17ca7
Rafal Szczesniak <rafal@likewise.com>
2009-02-16 23:12:09 +0000

    Fix warnings on Mac OS and AIX and remove mistakenly checked in code.
    Bug #7842
    
    (librpc: r31583)

--- librpc/TestRpc/Params.c
+++ librpc/TestRpc/Params.c
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include <config.h>
 
@@ -60,13 +61,7 @@ char* strndup(const char *s, size_t maxlen)
 
     return ret;
 }
-#else
-
-/* Some systems (like AIX) don't provide declaration unless
-   some macro magic is applied */
-char* strndup(const char *s, size_t maxlen);
-
-#endif /* !defined(HAVE_STRNDUP) */
+#endif
 
 
 /*
