a1d85d3fcfb3c56c47f1fde0588791c10e4d1de2
Krishna Ganugapati <krishnag@likewise.com>
2009-02-18 00:23:13 +0000

    Add libshareinfo linking to srv driver.
    (lwio: r31690)

--- lwio/libshareinfo/lwshareinfo.c
+++ lwio/libshareinfo/lwshareinfo.c
@@ -49,37 +49,6 @@
 
 #include "includes.h"
 
-
-typedef union _SHARE_INFO_UNION
-{
-    PSHARE_INFO_0   p0;
-    PSHARE_INFO_1   p1;
-    PSHARE_INFO_501 p501;
-    PSHARE_INFO_502 p502;
-} SHARE_INFO_UNION, *PSHARE_INFO_UNION;
-
-
-typedef struct _SHARE_INFO_ADD_PARAMS
-{
-    DWORD dwInfoLevel;
-    SHARE_INFO_UNION info;
-} SHARE_INFO_ADD_PARAMS, *PSHARE_INFO_ADD_PARAMS;
-
-typedef struct _SHARE_INFO_DELETE_PARAMS
-{
-    PWSTR servername;
-    PWSTR netname;
-    DWORD reserved;
-}SHARE_INFO_DELETE_PARAMS, *PSHARE_INFO_DELETE_PARAMS;
-
-typedef struct _SHARE_INFO_SETINFO_PARAMS
-{
-    PWSTR servername;
-    PWSTR netname;
-    DWORD level;
-    SHARE_INFO_UNION info;
-}SHARE_INFO_SETINFO_PARAMS, *PSHARE_INFO_SETINFO_PARAMS;
-
 static LWMsgTypeSpec gShareInfo0Spec[] =
 {
     LWMSG_STRUCT_BEGIN(SHARE_INFO_502),
@@ -215,7 +184,7 @@ LwShareInfoUnmarshalAddParameters(
     )
 {
     NTSTATUS Status = STATUS_SUCCESS;
-    PSHARE_INFO_ADD_PARAMS pParams hareinareinfo NULL;
+    PSHARE_INFO_ADD_PARAMS pParams = NULL;
     LWMsgContext* pContext = NULL;
 
     Status = MAP_LWMSG_STATUS(
