d25326f3ae1da230865967ddc9b9efc45701300c
Krishna Ganugapati <krishnag@likewise.com>
2009-03-07 00:29:01 +0000

    More cleanup
    (lsass: r32240)

--- lsass/server/rpc/dsapi/directory.h
+++ lsass/server/rpc/dsapi/directory.h
@@ -1,9 +1,23 @@
+#define  TYPE_BOOLEAN           1
+#define  TYPE_INTEGER           2
+#define  TYPE_LARGE_INTEGER     3
+#define  TYPE_NT_SECURITY_DESCRIPTOR
+#define  TYPE_OCTET_STRING      5
+#define  TYPE_PRINTABLE_STRING  6
+
+
+typedef struct _OCTET_STRING {
+    ULONG ulNumBytes;
+    PBYTE pBytes
+} OCTET_STRING, *POCTET_STRING;
+
 typedef struct _ATTRIBUTE_VALUE {
     ULONG Type;
     union {
         ULONG uLongValue;
         PWSTR pszStringValue;
         BOOL  bBooleanValue;
+        POCTET_STRING pOctetString;
     }
 } ATTRIBUTE_VALUE, *PATTRIBUTE_VALUE;
 
@@ -26,6 +40,19 @@ typedef struct _DIRECTORY_ENTRY{
     ULONG ulNumAttributes;
     PDIRECTORY_ATTRIBUTE * ppDirectoryAttributes;
 }DIRECTORY_ENTRY, *PDIRECTORY_ENTRY;
+
+#define ENTRY(i, ppDirectoryEntry) = *(ppDirectoryEntry + i)
+#define ATTRIBUTE(i, ppDirectoryAttributes) = *(ppDirectoryAttributes + i)
+
+#define VALUE(i, pAttribute) = *(pAttribute->ppAttributeValues + i)
+#define NUM_VALUES(pAttribute) = pAttribute->ulNumValues
+#define INTEGER(pValue) =  pValue->uLongValue;
+#define PRINTABLE_STRING(pValue) = pValue->pPrintableString;
+#define IA5_STRING(pValue) = pValue->pIA5String;
+#define OCTET_STRING_LENGTH(pValue) = pValue->pOctetString->ulNumBytes;
+#define OCTET_STRING_DATA(pValue) = pValue->pOctetString->pBytes;
+#define NT_SECURITY_DESCRIPTOR_LENGTH(pValue) = pValue->pNTSecurityDescriptor->ulNumBytes;
+#define NT_SECURITY_DESCRIPTOR_DATA(pValue) = pValue->pNTSecurityDescriptor->pBytes;
 
 NTSTATUS
 DirectoryAddObject(
