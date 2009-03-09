a5ef62ee68c344655dbc605cb2ed8f727d98b922
Krishna Ganugapati <krishnag@likewise.com>
2009-03-09 21:31:53 +0000

    SamDBInitGroupTables
    (lsass: r32261)

---
+++ lsass/server/rpc/samdb/samdbgroup.c
@@ -0,0 +1,61 @@
+
+#define DB_QUERY_CREATE_GROUPMEMBERSHIP_TABLE                  \
+    "create table lwigroupmembers (Gid integer,                \
+                                   Uid integer                 \
+                                  )"
+
+#define DB_QUERY_CREATE_GROUPS_TABLE                           \
+    "create table lwigroups (GroupRecordId integer PRIMARY KEY,\
+                             Name          varchar(256),       \
+                             Passwd        varchar(256),       \
+                             Gid           integer,            \
+                             CreatedTime   date                \
+                            )"
+
+#define DB_QUERY_CREATE_GROUPS_INSERT_TRIGGER                  \
+    "create trigger lwigroups_createdtime                      \
+     after insert on lwigroups                                 \
+     begin                                                     \
+          update lwigroups set CreatedTime = DATETIME('NOW')   \
+          where rowid = new.rowid;                             \
+     end"
+
+#define DB_QUERY_CREATE_GROUPS_DELETE_TRIGGER                  \
+    "create trigger lwigroups_delete_record                    \
+     after delete on lwigroups                                 \
+     begin                                                     \
+          delete from lwigroupmembers where Gid = old.Gid;     \
+     end"
+
+NTSTATUS
+SamDbInitGroupTable()
+{
+    dwError = sqlite3_exec(pDbHandle,
+                           DB_QUERY_CREATE_GROUPMEMBERSHIP_TABLE,
+                           NULL,
+                           NULL,
+                           &pszError);
+    BAIL_ON_LSA_ERROR(dwError);
+
+    dwError = sqlite3_exec(pDbHandle,
+                           DB_QUERY_CREATE_GROUPS_TABLE,
+                           NULL,
+                           NULL,
+                           &pszError);
+    BAIL_ON_LSA_ERROR(dwError);
+
+    dwError = sqlite3_exec(pDbHandle,
+                           DB_QUERY_CREATE_GROUPS_INSERT_TRIGGER,
+                           NULL,
+                           NULL,
+                           &pszError);
+    BAIL_ON_LSA_ERROR(dwError);
+
+    dwError = sqlite3_exec(pDbHandle,
+                           DB_QUERY_CREATE_GROUPS_DELETE_TRIGGER,
+                           NULL,
+                           NULL,
+                           &pszError);
+    BAIL_ON_LSA_ERROR(dwError);
+
+}
