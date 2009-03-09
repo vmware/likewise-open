3ae118a38fcdaa81bb52eef64200bb0511837043
Krishna Ganugapati <krishnag@likewise.com>
2009-03-09 21:31:28 +0000

    Add SamDBInitUserTable
    (lsass: r32260)

--- lsass/server/rpc/samdb/samdbuser.c
+++ lsass/server/rpc/samdb/samdbuser.c
@@ -1,4 +1,53 @@
+/* Editor Settings: expandtabs and use 4 spaces for indentation
+ * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
+ * -*- mode: c, c-basic-offset: 4 -*- */
 
+/*
+ * Copyright Likewise Software
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.  You should have received a copy of the GNU General
+ * Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * LIKEWISE SOFTWARE MAKES THIS SOFTWARE AVAILABLE UNDER OTHER LICENSING
+ * TERMS AS WELL.  IF YOU HAVE ENTERED INTO A SEPARATE LICENSE AGREEMENT
+ * WITH LIKEWISE SOFTWARE, THEN YOU MAY ELECT TO USE THE SOFTWARE UNDER THE
+ * TERMS OF THAT SOFTWARE LICENSE AGREEMENT INSTEAD OF THE TERMS OF THE GNU
+ * GENERAL PUBLIC LICENSE, NOTWITHSTANDING THE ABOVE NOTICE.  IF YOU
+ * HAVE QUESTIONS, OR WISH TO REQUEST A COPY OF THE ALTERNATE LICENSING
+ * TERMS OFFERED BY LIKEWISE SOFTWARE, PLEASE CONTACT LIKEWISE SOFTWARE AT
+ * license@likewisesoftware.com
+ */
+
+
+
+/*
+ * Copyright (C) Likewise Software. All rights reserved.
+ *
+ * Module Name:
+ *
+ *        samdbuser.c
+ *
+ * Abstract:
+ *
+ *
+ *      Likewise SAM Database Provider
+ *
+ *      SamDbInitUserTable
+ *
+ * Authors: Krishna Ganugapati (krishnag@likewisesoftware.com)
+ *
+ */
+#include "includes.h"
 
 
 #define DB_QUERY_CREATE_USERS_TABLE                               \
@@ -24,6 +73,26 @@
                             NTOwf_4          integer,             \
                             CreatedTime      date                 \
                            )"
+
+
+#define DB_QUERY_CREATE_USERS_INSERT_TRIGGER                   \
+    "create trigger lwiusers_createdtime                       \
+     after insert on lwiusers                                  \
+     begin                                                     \
+          update lwiusers                                      \
+          set CreatedTime = DATETIME('NOW')                    \
+          where rowid = new.rowid;                             \
+                                                               \
+          insert into lwigroupmembers (Uid, Gid)               \
+          values (new.Uid, new.Gid);                           \
+     end"
+
+#define DB_QUERY_CREATE_USERS_DELETE_TRIGGER                   \
+    "create trigger lwiusers_delete_record                     \
+     after delete on lwiusers                                  \
+     begin                                                     \
+          delete from lwigroupmembers where Uid = old.Uid;     \
+     end"
 
 
 NTSTATUS
