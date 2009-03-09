2e21d6d7ef761eb2fa7ad7cb7368a85fd41bedb2
Gerald Carter <gcarter@likewise.com>
2009-03-09 03:22:17 +0000

    npfs: Remove an pPipeList in favor or the existing pPipes
    (lwio: r32246)

--- lwio/server/npvfs/structs.h
+++ lwio/server/npvfs/structs.h
@@ -63,7 +63,6 @@ typedef struct _NPFS_FCB
     NPFS_INTERLOCKED_ULONG cRef;
 
     pthread_rwlock_t       PipeListRWLock;
-    PNPFS_PIPE             pPipeList;
     UNICODE_STRING         PipeName;
     ULONG                  NamedPipeConfiguration;
     FILE_PIPE_TYPE_MASK    NamedPipeType;
