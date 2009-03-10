2b6e7a441effda7b1bbb18e51037a58d9083d10d
Krishna Ganugapati <krishnag@likewise.com>
2009-03-10 21:34:32 +0000

    Add open/close functions
    (lsass: r32299)

--- lsass/server/store/samdb/samdbclose.c
+++ lsass/server/store/samdb/samdbclose.c
@@ -7,5 +7,11 @@ SamDbClose(
 {
     DWORD dwError = 0;
 
+    sqlite3* pDbHandle = (sqlite3*)hDb;
+
+    if (pDbHandle) {
+       sqlite3_close(pDbHandle);
+    }
+
     return dwError;
 }
--- lsass/server/store/samdb/samdbopen.c
+++ lsass/server/store/samdb/samdbopen.c
@@ -6,6 +6,24 @@ SamDbOpen(
     )
 {
     DWORD dwError = 0;
+    sqlite3* pDbHandle = NULL;
+
+    dwError = sqlite3_open(SAM_DB, &pDbHandle);
+    BAIL_ON_LSA_ERROR(dwError);
+
+    *phDb = (HANDLE)pDbHandle;
+
+cleanup:
 
     return dwError;
+
+error:
+
+    *(phDb) = (HANDLE)NULL;
+
+    if (pDbHandle) {
+        sqlite3_close(pDbHandle);
+    }
+
+    goto cleanup;
 }
