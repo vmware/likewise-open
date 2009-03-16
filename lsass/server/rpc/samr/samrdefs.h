c8a4cfbd46ea4d01dcb28aebcdff1c2e14b7ef58
Rafal Szczesniak <rafal@likewise.com>
2009-03-16 11:14:59 +0000

    Add first context handle type for SAM connections.
    (lsass: r32440)

---
+++ lsass/server/rpc/samr/samrdefs.h
@@ -0,0 +1,21 @@
+#ifndef _SAMRDEFS_H_
+#define _SAMRDEFS_H_
+
+#ifdef _DCE_IDL_
+typedef [context_handle] void* CONNECT_HANDLE;
+#else
+typedef void* CONNECT_HANDLE;
+#endif
+
+
+#endif /* _SAMRDEFS_H_ */
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
