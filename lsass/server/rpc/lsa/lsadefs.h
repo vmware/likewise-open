5dbab366691fe9d7cd839d903cb2544226db605a
Rafal Szczesniak <rafal@likewise.com>
2009-03-16 14:46:18 +0000

    Define POLICY_HANDLE as basic context handle in lsarpc.
    (lsass: r32448)

---
+++ lsass/server/rpc/lsa/lsadefs.h
@@ -0,0 +1,21 @@
+#ifndef _SRV_LSADEFS_H_
+#define _SRV_LSADEFS_H_
+
+#ifdef _DCE_IDL_
+typedef [context_handle] void* POLICY_HANDLE;
+#else
+typedef void* POLICY_HANDLE;
+#endif
+
+
+#endif /* _SRV_LSADEFS_H_ */
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
