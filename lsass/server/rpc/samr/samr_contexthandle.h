2e2b352244e6aad22cad377170de5e108a68ad89
Rafal Szczesniak <rafal@likewise.com>
2009-03-17 19:39:09 +0000

    Add missing header.
    (lsass: r32517)

---
+++ lsass/server/rpc/samr/samr_contexthandle.h
@@ -0,0 +1,20 @@
+#ifndef _SAMR_CONNECT_HANDLE_H_
+#define _SAMR_CONNECT_HANDLE_H_
+
+
+typedef struct samr_connect_context {
+    PACCESS_TOKEN pUserToken;
+} CONNECT_CONTEXT, *PCONNECT_CONTEXT;
+
+
+#endif /* _SAMR_CONNECT_HANDLE_H_ */
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
