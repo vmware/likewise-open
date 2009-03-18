389cc02c164fe190a5d17f71cafd486bd5c8ba35
Rafal Szczesniak <rafal@likewise.com>
2009-03-18 22:01:52 +0000

    Fix bug #8332.
    Try to change affinity if NetrGetDomainInfo fails with access denied even though
    schannel has been successfully established. This helps to deal with DCs that
    are "half up".
    
    Merge to 5.1 branch is waiting for code review.
    
    (lsass: r32593)

--- lsass/server/auth-providers/ad-provider/adnetapi.c
+++ lsass/server/auth-providers/ad-provider/adnetapi.c
@@ -1,6 +1,6 @@
 /* Editor Settings: expandtabs and use 4 spaces for indentation
  * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
- * -*- mode: c, c-basic-offset: 4 -*- */
+ */
 
 /*
  * Copyright Likewise Software    2004-2008
@@ -45,13 +45,12 @@
  *          Sriram Nambakam (snambakam@likewisesoftware.com)
  *          Wei Fu (wfu@likewisesoftware.com)
  */
+
 #include "adprovider.h"
 #include "adnetapi.h"
 
 static NetrCredentials gSchannelCreds = { 0 };
 static NetrCredentials* gpSchannelCreds = NULL;
-static NETRESOURCE gSchannelRes = { 0 };
-static NETRESOURCE* gpSchannelRes = NULL;
 static handle_t ghSchannelBinding = NULL;
 static pthread_mutex_t gSchannelLock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -1317,13 +1316,15 @@ error:
 
 DWORD
 AD_NetlogonAuthenticationUserEx(
-    IN PSTR pszDomainController,
+    IN PSTR pszDCName,
     IN PLSA_AUTH_USER_PARAMS pUserParams,
     OUT PLSA_AUTH_USER_INFO *ppUserInfo,
     OUT PBOOLEAN pbIsNetworkError
     )
 {
     DWORD dwError = LSA_ERROR_INTERNAL;
+    PSTR pszDomainController = NULL;
+    PSTR pszDnsDomainName = NULL;
     PWSTR pwszDomainController = NULL;
     PWSTR pwszServerName = NULL;
     PWSTR pwszShortDomain = NULL;
@@ -1347,6 +1348,13 @@ AD_NetlogonAuthenticationUserEx(
     LW_PIO_ACCESS_TOKEN pAccessToken = NULL;
     LW_PIO_ACCESS_TOKEN pOldToken = NULL;
     BOOLEAN bChangedToken = FALSE;
+    DWORD dwDomainInfoLevel = 0;
+    NetrDomainQuery DomainQuery;
+    NetrDomainQuery1 DomainQuery1;
+    NetrDomainInfo *pDomainInfo = NULL;
+    PWSTR pwszSiteName = NULL;
+    PLWNET_DC_INFO pDcInfo = NULL;
+    DWORD dwSchnAttempt = 0;
 
     pthread_mutex_lock(&gSchannelLock);
 
@@ -1366,6 +1374,9 @@ AD_NetlogonAuthenticationUserEx(
 
     /* Gather other Schannel params */
 
+    dwError = LsaAllocateString(pszDCName, &pszDomainController);
+    BAIL_ON_LSA_ERROR(dwError);
+
     /* Allocate space for the servername.  Include room for the terminating
        NULL and \\ */
 
@@ -1383,13 +1394,20 @@ AD_NetlogonAuthenticationUserEx(
     dwError = LsaMbsToWc16s(pUserParams->pszDomain, &pwszShortDomain);
     BAIL_ON_LSA_ERROR(dwError);
 
+    dwError = LsaWc16sToMbs(pMachAcctInfo->pwszDnsDomainName,
+                            &pszDnsDomainName);
+    BAIL_ON_LSA_ERROR(dwError);
+
     dwError = LsaKrb5GetSystemCachePath(KRB5_File_Cache, &pszCcachePath);
     BAIL_ON_LSA_ERROR(dwError);
 
     dwError = LsaMbsToWc16s(pszCcachePath, &pwszCcachePath);
     BAIL_ON_LSA_ERROR(dwError);
 
-    if (!ghSchannelBinding)
+    /* Try to force reaffinity 5 times at most, if there's need */
+    dwSchnAttempt = 5;
+
+    while (!ghSchannelBinding)
     {
         /* Establish the initial bind to \NETLOGON */
 
@@ -1401,7 +1419,8 @@ AD_NetlogonAuthenticationUserEx(
         dwError = LwNtStatusToUnixErrno(status);
         BAIL_ON_LSA_ERROR(dwError);
 
-        status = InitNetlogonBindingDefault(&netr_b,(const char*)pszDomainController, pAccessToken, FALSE);
+        status = InitNetlogonBindingDefault(&netr_b, pszDomainController,
+                                            pAccessToken, FALSE);
         if (status != 0)
         {
             LSA_LOG_DEBUG("Failed to bind to %s (error %d)",
@@ -1429,8 +1448,83 @@ AD_NetlogonAuthenticationUserEx(
             BAIL_ON_LSA_ERROR(dwError);
         }
 
+        memset(&DomainQuery, 0, sizeof(DomainQuery));
+        memset(&DomainQuery1, 0, sizeof(DomainQuery1));
+
+        dwDomainInfoLevel  = 1;
+        DomainQuery.query1 = &DomainQuery1;
+
+        /* As it turns out workstation_site can be NULL */
+        DomainQuery1.workstation_domain = pwszShortDomain;
+        DomainQuery1.workstation_site   = pwszSiteName;
+
+        nt_status = NetrGetDomainInfo(ghSchannelBinding,
+                                      &gSchannelCreds,
+                                      pwszServerName,
+                                      pMachAcctInfo->pwszHostname,
+                                      dwDomainInfoLevel,
+                                      &DomainQuery,
+                                      &pDomainInfo);
+
+        if (nt_status != STATUS_SUCCESS &&
+            nt_status != STATUS_ACCESS_DENIED)
+        {
+            dwError = LSA_ERROR_RPC_ERROR;
+            BAIL_ON_LSA_ERROR(dwError);
+        }
+
+        if (nt_status == STATUS_ACCESS_DENIED &&
+            dwSchnAttempt > 0)
+        {
+            /* Remote netlogon service seems to be "partly up" so we're
+               going to try change affinity */
+
+            NetrCloseSchannel(ghSchannelBinding);
+            ghSchannelBinding = NULL;
+
+            FreeNetlogonBinding(&netr_b);
+            netr_b = NULL;
+
+            LSA_SAFE_FREE_MEMORY(pwszSiteName);
+            LSA_SAFE_FREE_MEMORY(pszDomainController);
+            LSA_SAFE_FREE_STRING(pszServerName);
+            LSA_SAFE_FREE_MEMORY(pwszServerName);
+            LSA_SAFE_FREE_MEMORY(pszDomainController);
+
+            dwError = LWNetGetDCName(NULL,
+                                     pszDnsDomainName,
+                                     NULL,
+                                     DS_FORCE_REDISCOVERY,
+                                     &pDcInfo);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            dwError = LsaMbsToWc16s(pDcInfo->pszDCSiteName, &pwszSiteName);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            dwError = LsaAllocateString(pDcInfo->pszDomainControllerName,
+                                        &pszDomainController);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            dwDCNameLen = strlen(pszDomainController) + 3;
+            dwError = LsaAllocateMemory(dwDCNameLen, (PVOID*)&pszServerName);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            snprintf(pszServerName, dwDCNameLen, "\\\\%s", pszDomainController);
+            dwError = LsaMbsToWc16s(pszServerName, &pwszServerName);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            LWNetFreeDCInfo(pDcInfo);
+            dwSchnAttempt--;
+
+        }
+        else if (nt_status == STATUS_ACCESS_DENIED &&
+                 dwSchnAttempt == 0)
+        {
+            dwError = LSA_ERROR_RPC_ERROR;
+            BAIL_ON_LSA_ERROR(dwError);
+        }
+
         gpSchannelCreds = &gSchannelCreds;
-        gpSchannelRes   = &gSchannelRes;
     }
 
     /* Time to do the authentication */
@@ -1510,6 +1604,8 @@ cleanup:
         LwIoDeleteAccessToken(pAccessToken);
     }
 
+    LSA_SAFE_FREE_MEMORY(pszDomainController);
+    LSA_SAFE_FREE_MEMORY(pszDnsDomainName);
     LSA_SAFE_FREE_MEMORY(pwszDomainController);
     LSA_SAFE_FREE_MEMORY(pwszServerName);
     LSA_SAFE_FREE_MEMORY(pwszShortDomain);
@@ -1543,13 +1639,11 @@ AD_ClearSchannelState(
 
         memset(&gSchannelCreds, 0, sizeof(gSchannelCreds));
         gpSchannelCreds = NULL;
-
-        memset(&gSchannelRes, 0, sizeof(gSchannelRes));
-        gpSchannelRes = NULL;
     }
 
     pthread_mutex_unlock(&gSchannelLock);
 }
+
 
 /*
 local variables:
