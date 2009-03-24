1d0de3495d0a3c37edc45d0eb334a1a6d2bf3e6b
Rafal Szczesniak <rafal@likewise.com>
2009-03-24 14:34:59 +0000

    Add implementation of SamrLookupNames function.
    (lsass: r32733)

--- lsass/server/rpc/samr/samr_lookupnames.c
+++ lsass/server/rpc/samr/samr_lookupnames.c
@@ -29,7 +29,17 @@
  */
 
 /*
- * Abstract: SamrLookupNames function (rpc server library)
+ * Copyright (C) Likewise Software. All rights reserved.
+ *
+ * Module Name:
+ *
+ *        samr_lookupnames.c
+ *
+ * Abstract:
+ *
+ *        Remote Procedure Call (RPC) Server Interface
+ *
+ *        SamrLookupNames function
  *
  * Authors: Rafal Szczesniak (rafal@likewise.com)
  */
@@ -39,19 +49,232 @@
 
 NTSTATUS
 SamrLookupNames(
-    /* [in] */ PolicyHandle *domain_handle,
+    /* [in] */ handle_t hBinding,
+    /* [in] */ DOMAIN_HANDLE hDomain,
     /* [in] */ uint32 num_names,
     /* [in] */ UnicodeString *names,
     /* [out] */ Ids *ids,
     /* [out] */ Ids *types
     )
 {
-    NTSTATUS status = STATUS_NOT_IMPLEMENTED;
+    CHAR szFilterFmt[] = "(|(&(objectclass=user)(user-name=%S))"
+                           "(&(objectclass=group)(group-name=%S)))";
+
+    NTSTATUS status = STATUS_SUCCESS;
+    DWORD dwError = 0;
+    PDOMAIN_CONTEXT pDomCtx = NULL;
+    HANDLE hDirectory = NULL;
+    Ids *pIds = NULL;
+    Ids *pTypes = NULL;
+    PWSTR pwszDn = NULL;
+    DWORD dwScope = 0;
+    PWSTR pwszFilter = NULL;
+    PWSTR wszAttributes[3];
+    PWSTR pwszAttrNameGid = NULL;
+    PWSTR pwszAttrNameUid = NULL;
+    DWORD i = 0;
+    PWSTR pwszName = NULL;
+    PDIRECTORY_ENTRY pEntries = NULL;
+    PDIRECTORY_ENTRY pEntry = NULL;
+    PDIRECTORY_ATTRIBUTE pAttr = NULL;
+    PATTRIBUTE_VALUE pAttrVal = NULL;
+    DWORD dwEntriesNum = 0;
+
+    pDomCtx = (PDOMAIN_CONTEXT)hDomain;
+
+    if (pDomCtx == NULL || pDomCtx->Type != SamrContextDomain) {
+        status = STATUS_INVALID_HANDLE;
+        BAIL_ON_NTSTATUS_ERROR(status);
+    }
+
+    hDirectory = pDomCtx->pConnCtx->hDirectory;
+    pwszDn     = pDomCtx->pwszDn;
+
+    status = SamrSrvAllocateMemory((void**)&(pIds),
+                                   sizeof(*pIds),
+                                   NULL);
+    BAIL_ON_NTSTATUS_ERROR(status);
+
+    pIds->count = num_names;
+    status = SamrSrvAllocateMemory((void**)&(pIds->ids),
+                                   pIds->count * sizeof(uint32),
+                                   NULL);
+    BAIL_ON_NTSTATUS_ERROR(status);
+
+    status = SamrSrvAllocateMemory((void**)&(pTypes),
+                                   sizeof(*pTypes),
+                                   NULL);
+    BAIL_ON_NTSTATUS_ERROR(status);
+
+    pTypes->count = num_names;
+    status = SamrSrvAllocateMemory((void**)&(pTypes->ids),
+                                   pTypes->count * sizeof(uint32),
+                                   NULL);
+    BAIL_ON_NTSTATUS_ERROR(status);
+
+    dwError = LsaMbsToWc16s(DIRECTORY_ATTR_TAG_GID, &pwszAttrNameGid);
+    BAIL_ON_LSA_ERROR(dwError);
+
+    dwError = LsaMbsToWc16s(DIRECTORY_ATTR_TAG_UID, &pwszAttrNameUid);
+    BAIL_ON_LSA_ERROR(dwError);
+
+    for (i = 0; i < num_names; i++) {
+        UnicodeString *name = &(names[i]);
+
+        status = SamrSrvAllocateMemory((void**)&pwszName,
+                                       name->size * sizeof(WCHAR),
+                                       NULL);
+        BAIL_ON_NTSTATUS_ERROR(status);
+
+        wc16sncpy(pwszName, name->string, name->len);
+
+        status = SamrSrvAllocateMemory((void**)&pwszFilter,
+                                       (name->size * 2 + sizeof(szFilterFmt))
+                                       * sizeof(WCHAR),
+                                       NULL);
+        BAIL_ON_NTSTATUS_ERROR(status);
+
+        sw16printf(pwszFilter, szFilterFmt, pwszName);
+
+        wszAttributes[0] = pwszAttrNameUid;
+        wszAttributes[1] = pwszAttrNameGid;
+        wszAttributes[2] = NULL;
+
+        pEntry   = NULL;
+        pAttr    = NULL;
+        pAttrVal = NULL;
+
+        dwError = DirectorySearch(hDirectory,
+                                  pwszDn,
+                                  dwScope,
+                                  pwszFilter,
+                                  wszAttributes,
+                                  TRUE,
+                                  &pEntries,
+                                  &dwEntriesNum);
+        BAIL_ON_LSA_ERROR(dwError);
+
+        if (dwEntriesNum == 1) {
+            pEntry = &(pEntries[0]);
+
+            if (pEntry->ulNumAttributes) {
+                dwError = DirectoryGetEntryAttributeByName(pEntry,
+                                                           pwszAttrNameUid,
+                                                           &pAttr);
+                if (dwError == 0) {
+                    dwError = DirectoryGetAttributeValue(pAttr,
+                                                         &pAttrVal);
+                    BAIL_ON_LSA_ERROR(dwError);
+
+                    if (pAttrVal->Type == DIRECTORY_ATTR_TYPE_LARGE_INTEGER) {
+                        pIds->ids[i]   = pAttrVal->uLongValue;
+                        pTypes->ids[i] = SID_TYPE_USER;
+
+                    } else {
+                        status = STATUS_INTERNAL_ERROR;
+                        BAIL_ON_NTSTATUS_ERROR(status);
+                    }
+
+                } else {
+                    dwError = DirectoryGetEntryAttributeByName(pEntry,
+                                                               pwszAttrNameGid,
+                                                               &pAttr);
+                    BAIL_ON_LSA_ERROR(dwError);
+
+                    dwError = DirectoryGetAttributeValue(pAttr,
+                                                         &pAttrVal);
+                    BAIL_ON_LSA_ERROR(dwError);
+
+                    if (pAttrVal->Type == DIRECTORY_ATTR_TYPE_LARGE_INTEGER) {
+                        pIds->ids[i]   = pAttrVal->uLongValue;
+                        pTypes->ids[i] = SID_TYPE_ALIAS;
+
+                    } else {
+                        status = STATUS_INTERNAL_ERROR;
+                        BAIL_ON_NTSTATUS_ERROR(status);
+                    }
+                }
+
+            } else {
+                status = STATUS_INTERNAL_ERROR;
+                BAIL_ON_NTSTATUS_ERROR(status);
+            }
+
+        } else if (dwEntriesNum == 0) {
+            pIds->ids[i]   = 0;
+            pTypes->ids[i] = SID_TYPE_UNKNOWN;
+
+        } else {
+            status = STATUS_INTERNAL_ERROR;
+            BAIL_ON_NTSTATUS_ERROR(status);
+        }
+
+        if (pEntries) {
+            DirectoryFreeEntries(pEntries, dwEntriesNum);
+            pEntries = NULL;
+        }
+
+        if (pwszFilter) {
+            SamrSrvFreeMemory(pwszFilter);
+            pwszFilter = NULL;
+        }
+
+        if (pwszName) {
+            SamrSrvFreeMemory(pwszName);
+            pwszName = NULL;
+        }
+    }
+
+    ids->count   = pIds->count;
+    ids->ids     = pIds->ids;
+    types->count = pTypes->count;
+    types->ids   = pTypes->ids;
 
 cleanup:
+    if (pwszAttrNameGid) {
+        LSA_SAFE_FREE_MEMORY(pwszAttrNameGid);
+    }
+
+    if (pwszAttrNameGid) {
+        LSA_SAFE_FREE_MEMORY(pwszAttrNameUid);
+    }
+
+    if (pwszName) {
+        SamrSrvFreeMemory(pwszName);
+    }
+
+    if (pwszFilter) {
+        SamrSrvFreeMemory(pwszFilter);
+    }
+
+    if (pEntries) {
+        DirectoryFreeEntries(pEntries, dwEntriesNum);
+    }
+
+    if (pIds) {
+        SamrSrvFreeMemory(pIds);
+    }
+
+    if (pTypes) {
+        SamrSrvFreeMemory(pTypes);
+    }
+
     return status;
 
 error:
+    if (pIds->ids) {
+        SamrSrvFreeMemory(pIds->ids);
+    }
+
+    if (pTypes->ids) {
+        SamrSrvFreeMemory(pTypes->ids);
+    }
+
+    ids->count   = 0;
+    ids->ids     = NULL;
+    types->count = 0;
+    types->ids   = NULL;
+
     goto cleanup;
 }
 
