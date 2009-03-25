020a07a43ed8c7aeb0ab8efe15af11c39f6bb579
Sriram Nambakam <snambakam@likewise.com>
2009-03-25 21:26:20 +0000

    cleanup sam db schema
    (lsass: r32783)

--- lsass/server/store/samdb/dbtable.c
+++ lsass/server/store/samdb/dbtable.c
@@ -1,37 +1,41 @@
+#ifndef __SAM_DB_TABLE_H__
+#define __SAM_DB_TABLE_H__
 
-#define DB_QUERY_CREATE_OBJECT_TABLE  \
-    "create table sambdobjects (       \
-                ObjectRecordId     integer PRIMARY KEY, \
-                ObjectSID        text unique,         \
-                distinguishedName text unique       \
-                parentRecordId    integer
-                objectClass       integer
-                CreatedTime       date
-/* Domain Attributes
-                dc                text  \
-/* User and Group Attributes */
-                cn               text    \
-                samAccountName    text   unique             \
-                description       text   \
-/* user attributes */
-                Uid              integer,             \
-                UserPasswd           text,                \
-                UserInfoFlags    integer,             \
-                Gecos            text,                \
-                HomeDir          text,                \
-                Shell            text,                \
-                PasswdChangeTime integer,             \
-                FullName         text,                \
-                AccountExpiry    integer,             \
-                LMOwf_1          integer,             \
-                LMOwf_2          integer,             \
-                LMOwf_3          integer,             \
-                LMOwf_4          integer,             \
-                NTOwf_1          integer,             \
-                NTOwf_2          integer,             \
-                NTOwf_3          integer,             \
-                NTOwf_4          integer,             \
-/*Group Attributes*/
-                Gid            integer,             \
-                GroupPasswd         text,                \
-               )"
+#define SAM_DB_QUERY_CREATE_TABLES  \
+    "create table sambdobjects (\n" \
+                 "ObjectRecordId    integer PRIMARY KEY,\n" \
+                 "ObjectSID         text unique,\n"         \
+                 "DistinguishedName text unique,\n"         \
+                 "ParentDN          text,\n"                \
+                 "ObjectClass       integer,\n"             \
+                 "Domain            text,\n"                \
+                 "CommonName        text,\n"                \
+                 "SamAccountName    text unique,\n"         \
+                 "Description       text,\n"                \
+                 "UID               integer,\n"             \
+                 "UserPasswd        text,\n"                \
+                 "UserInfoFlags     integer,\n"             \
+                 "Gecos             text,\n"                \
+                 "HomeDir           text,\n"                \
+                 "Shell             text,\n"                \
+                 "PasswdChangeTime  integer,\n"             \
+                 "FullName          text,\n"                \
+                 "AccountExpiry     integer,\n"             \
+                 "LMOwf_1           integer,\n"             \
+                 "LMOwf_2           integer,\n"             \
+                 "LMOwf_3           integer,\n"             \
+                 "LMOwf_4           integer,\n"             \
+                 "NTOwf_1           integer,\n"             \
+                 "NTOwf_2           integer,\n"             \
+                 "NTOwf_3           integer,\n"             \
+                 "NTOwf_4           integer,\n"             \
+                 "GID               integer,\n"             \
+                 "GroupPasswd       text,\n"                \
+                 "CreatedTime       date\n"                 \
+                 ");\n"                                     \
+    "create table samdbmembers (\n"                         \
+                 "GroupRecordId groupId,\n"                 \
+                 "MemberRecordId memberId,\n"               \
+                 "CreatedTime    date\n)"
+
+#endif /* __SAM_DB_TABLE_H__ */
