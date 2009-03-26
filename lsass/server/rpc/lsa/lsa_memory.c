ecb3f4a58efa991254f318c5944afd30392df356
Rafal Szczesniak <rafal@likewise.com>
2009-03-26 13:38:32 +0000

    Ground work before adding LsaLookupNames implementation.
    Add LsaSrvAddDepMemory and LsaSrvReallocMemory functions.
    
    (lsass: r32833)

--- lsass/server/rpc/lsa/lsa_memory.c
+++ lsass/server/rpc/lsa/lsa_memory.c
@@ -1,3 +1,49 @@
+/* Editor Settings: expandtabs and use 4 spaces for indentation
+ * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
+ */
+
+/*
+ * Copyright Likewise Software    2004-2009
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the license, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.  You should have received a copy
+ * of the GNU Lesser General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ * LIKEWISE SOFTWARE MAKES THIS SOFTWARE AVAILABLE UNDER OTHER LICENSING
+ * TERMS AS WELL.  IF YOU HAVE ENTERED INTO A SEPARATE LICENSE AGREEMENT
+ * WITH LIKEWISE SOFTWARE, THEN YOU MAY ELECT TO USE THE SOFTWARE UNDER THE
+ * TERMS OF THAT SOFTWARE LICENSE AGREEMENT INSTEAD OF THE TERMS OF THE GNU
+ * LESSER GENERAL PUBLIC LICENSE, NOTWITHSTANDING THE ABOVE NOTICE.  IF YOU
+ * HAVE QUESTIONS, OR WISH TO REQUEST A COPY OF THE ALTERNATE LICENSING
+ * TERMS OFFERED BY LIKEWISE SOFTWARE, PLEASE CONTACT LIKEWISE SOFTWARE AT
+ * license@likewisesoftware.com
+ */
+
+/*
+ * Copyright (C) Likewise Software. All rights reserved.
+ *
+ * Module Name:
+ *
+ *        lsa_memeory.c
+ *
+ * Abstract:
+ *
+ *        Remote Procedure Call (RPC) Server Interface
+ *
+ *        Lsa memory allocation manager
+ *
+ * Authors: Rafal Szczesniak (rafal@likewise.com)
+ */
+
 #include "includes.h"
 
 
@@ -74,6 +120,61 @@ cleanup:
 
 error:
     *ppOut = NULL;
+    goto cleanup;
+}
+
+
+NTSTATUS
+LsaSrvReallocMemory(
+    void **ppOut,
+    DWORD dwNewSize,
+    void *pIn
+    )
+{
+    NTSTATUS status = STATUS_SUCCESS;
+    void *pOut = NULL;
+    int locked = 0;
+
+    GLOBAL_DATA_LOCK(locked);
+
+    pOut = trealloc(pIn, dwNewSize);
+    BAIL_ON_NO_MEMORY(pOut);
+
+    *ppOut = pOut;
+
+cleanup:
+    GLOBAL_DATA_UNLOCK(locked);
+
+    return status;
+
+error:
+    *ppOut = NULL;
+    goto cleanup;
+}
+
+
+NTSTATUS
+LsaSrvAddDepMemory(
+    void *pIn,
+    void *pDep
+    )
+{
+    NTSTATUS status = STATUS_SUCCESS;
+    void *pParent = NULL;
+    int locked = 0;
+
+    pParent = (pDep) ? pDep : pLsaSrvMemRoot;
+
+    GLOBAL_DATA_LOCK(locked);
+
+    tlink(pParent, pIn);
+
+cleanup:
+    GLOBAL_DATA_UNLOCK(locked);
+
+    return status;
+
+error:
     goto cleanup;
 }
 
