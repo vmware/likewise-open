4663c9e018eedbc84a1e3b3f259ca8b9ecc3f296
Krishna Ganugapati <krishnag@likewise.com>
2009-03-27 19:29:24 +0000

    More stuff
    (lsass: r32897)

---
+++ lsass/server/store/samdb/sammarshall.c
@@ -0,0 +1,20 @@
+#include "samdb.h"
+
+
+DWORD
+SamDbMarshallData(
+    PWSTR pszSqlQuery,
+    PSTR * ppszResult,
+    DWORD nRows,
+    DWORD nCols,
+    PDIRECTORY_ENTRY **ppDirectoryEntry,
+    DWORD * pdwNumEntries
+    )
+{
+    DWORD dwError = 0;
+
+    return(dwError);
+}
+
+
+
