8d443f54f801df6e1c0bdc6e93eef215ba510d7f
Rafal Szczesniak <rafal@likewise.com>
2009-03-27 20:05:00 +0000

    Add complete support for passing config file parameters to samr rpc server. Add first parameter - domain socket path for local procedure calls.
    (lsass: r32903)

---
+++ lsass/server/rpc/samr/samr_cfg.h
@@ -0,0 +1,56 @@
+#ifndef _SAMR_CFG_H_
+#define _SAMR_CFG_H_
+
+
+typedef struct samr_srv_config {
+    PSTR pszLpcSocketPath;
+} SAMR_SRV_CONFIG, *PSAMR_SRV_CONFIG;
+
+
+typedef DWORD (*pFnSamrSrvConfigHandler)(PSAMR_SRV_CONFIG pConfig,
+					 PCSTR pszName,
+					 PCSTR pszValue);
+
+
+typedef struct samr_config_handler {
+    PCSTR pszId;
+    pFnSamrSrvConfigHandler pFnHandler;
+} SAMR_SRV_CONFIG_HANDLER, *PSAMR_SRV_CONFIG_HANDLER;
+
+
+DWORD
+SamrSrvInitialiseConfig(
+    PSAMR_SRV_CONFIG pConfig
+    );
+
+
+DWORD
+SamrSrvParseConfigFile(
+    PCSTR pszConfigFilePath,
+    PSAMR_SRV_CONFIG pConfig
+    );
+
+
+DWORD
+SamrSrvConfigGetLpcSocketPath(
+    PSTR *ppszLpcSocketPath
+    );
+
+
+DWORD
+SamrSrvSetConfigFilePath(
+    PCSTR pszConfigFilePath
+    );
+
+
+#endif /* _SAMR_CFG_H_ */
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
