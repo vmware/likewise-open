bf7f53d3dea168f627fa86b5adadfcc9cdae180c
Krishna Ganugapati <krishnag@likewise.com>
2009-05-13 00:01:45 +0000

    More ntlm files
    (lsass: r33739)

---
+++ lsass/gssntlm2/gssntlm/ntlm_gss_process_token.c
@@ -0,0 +1,10 @@
+
+OM_uint32
+ntlm_gss_process_context_token(
+     OM_uint32 *minor_status,
+     gss_ctx_id_t context_handle,
+     gss_buffer_t token_buffer
+	)
+{
+    return GSS_S_UNAVAILABLE;
+}
