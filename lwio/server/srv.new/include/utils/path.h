6009f0f6a7df65cda9e92771c8c9e6ee47f6a3f3
Sriram Nambakam <snambakam@likewise.com>
2009-05-22 00:59:14 +0000

    lwio:srv.new:minor fixes
    (lwio: r34008)

--- lwio/server/srv.new/include/utils/path.h
+++ lwio/server/srv.new/include/utils/path.h
@@ -1,5 +1,50 @@
-#ifndef __SRV_PATH_H__
-#define __SRV_PATH_H__
+/*
+ * Copyright Likewise Software    2004-2009
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.  You should have received a copy of the GNU General
+ * Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * LIKEWISE SOFTWARE MAKES THIS SOFTWARE AVAILABLE UNDER OTHER LICENSING
+ * TERMS AS WELL.  IF YOU HAVE ENTERED INTO A SEPARATE LICENSE AGREEMENT
+ * WITH LIKEWISE SOFTWARE, THEN YOU MAY ELECT TO USE THE SOFTWARE UNDER THE
+ * TERMS OF THAT SOFTWARE LICENSE AGREEMENT INSTEAD OF THE TERMS OF THE GNU
+ * GENERAL PUBLIC LICENSE, NOTWITHSTANDING THE ABOVE NOTICE.  IF YOU
+ * HAVE QUESTIONS, OR WISH TO REQUEST A COPY OF THE ALTERNATE LICENSING
+ * TERMS OFFERED BY LIKEWISE SOFTWARE, PLEASE CONTACT LIKEWISE SOFTWARE AT
+ * license@likewisesoftware.com
+ */
+
+/*
+ * Copyright (C) Likewise Software. All rights reserved.
+ *
+ * Module Name:
+ *
+ *        utils/path.h
+ *
+ * Abstract:
+ *
+ *        Likewise Input Output (LWIO) - SRV
+ *
+ *        Utilities
+ *
+ *        Path
+ *
+ * Authors: Sriram Nambakam (snambakam@likewise.com)
+ *
+ */
+
+#ifndef __SRV_UTILS_PATH_H__
+#define __SRV_UTILS_PATH_H__
 
 NTSTATUS
 SrvBuildFilePath(
@@ -8,4 +53,4 @@ SrvBuildFilePath(
     PWSTR* ppwszFilename
     );
 
-#endif /* __SRV_PATH_H__ */
+#endif /* __SRV_UTILS_PATH_H__ */
