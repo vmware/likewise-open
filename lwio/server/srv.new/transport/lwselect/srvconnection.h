acda3a7fa9e446d99566c3ad6376794bdb23f2be
Sriram Nambakam <snambakam@likewise.com>
2009-06-11 07:35:15 +0000

    lwio:srv.new: cleanup header
    (lwio: r34477)

--- lwio/server/srv.new/transport/lwselect/srvconnection.h
+++ lwio/server/srv.new/transport/lwselect/srvconnection.h
@@ -13,27 +13,6 @@ SrvConnectionGetNextSequence(
     PULONG              pulRequestSequence
     );
 
-BOOLEAN
-SrvConnectionIsInvalid(
-    PLWIO_SRV_CONNECTION pConnection
-    );
-
-VOID
-SrvConnectionSetInvalid(
-    PLWIO_SRV_CONNECTION pConnection
-    );
-
-LWIO_SRV_CONN_STATE
-SrvConnectionGetState(
-    PLWIO_SRV_CONNECTION pConnection
-    );
-
-VOID
-SrvConnectionSetState(
-    PLWIO_SRV_CONNECTION pConnection,
-    LWIO_SRV_CONN_STATE  connState
-    );
-
 NTSTATUS
 SrvConnectionReadPacket(
     PLWIO_SRV_CONNECTION pConnection,
@@ -45,31 +24,6 @@ SrvConnectionWriteMessage(
     PLWIO_SRV_CONNECTION pConnection,
     ULONG               ulSequence,
     PSMB_PACKET         pPacket
-    );
-
-NTSTATUS
-SrvConnectionFindSession(
-    PLWIO_SRV_CONNECTION pConnection,
-    USHORT uid,
-    PLWIO_SRV_SESSION* ppSession
-    );
-
-NTSTATUS
-SrvConnectionRemoveSession(
-    PLWIO_SRV_CONNECTION pConnection,
-    USHORT              uid
-    );
-
-NTSTATUS
-SrvConnectionCreateSession(
-    PLWIO_SRV_CONNECTION pConnection,
-    PLWIO_SRV_SESSION* ppSession
-    );
-
-NTSTATUS
-SrvConnectionGetNamedPipeSessionKey(
-    PLWIO_SRV_CONNECTION pConnection,
-    PIO_ECP_LIST        pEcpList
     );
 
 NTSTATUS
