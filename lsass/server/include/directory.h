09fa3a389d442fda6209dc29bbfce4bff85db62c
Rafal Szczesniak <rafal@likewise.com>
2009-07-14 19:10:08 +0000

    Add object classes and attributes to enable accessing database objects using DSAPI.
    Bug #8888.
    
    (lsass: r35317)

--- lsass/server/include/directory.h
+++ lsass/server/include/directory.h
@@ -126,6 +126,128 @@ typedef struct _DIRECTORY_MOD
 #define NT_SECURITY_DESCRIPTOR_LENGTH(pValue) = pValue->pNTSecurityDescriptor->ulNumBytes;
 #define NT_SECURITY_DESCRIPTOR_DATA(pValue) = pValue->pNTSecurityDescriptor->pBytes;
 
+#define DIR_OBJECT_CLASS_UNKNOWN         (0)
+#define DIR_OBJECT_CLASS_DOMAIN          (1)
+#define DIR_OBJECT_CLASS_BUILTIN_DOMAIN  (2)
+#define DIR_OBJECT_CLASS_CONTAINER       (3)
+#define DIR_OBJECT_CLASS_LOCAL_GROUP     (4)
+#define DIR_OBJECT_CLASS_USER            (5)
+#define DIR_OBJECT_CLASS_LOCALGRP_MEMBER (6)
+#define DIR_OBJECT_CLASS_SENTINEL        (7)
+
+
+#define DIRECTORY_ATTR_RECORD_ID \
+    {'O','b','j','e','c','t','R','e','c','o','r','d','I','d',0}
+#define DIRECTORY_ATTR_OBJECT_SID \
+    {'O','b','j','e','c','t','S','I','D',0}
+#define DIRECTORY_ATTR_DISTINGUISHED_NAME  \
+    {'D','i','s','t','i','n','g','u','i','s','h','e','d','N','a','m','e',0}
+#define DIRECTORY_ATTR_PARENT_DN \
+    {'P','a','r','e','n','t','D','N',0}
+#define DIRECTORY_ATTR_OBJECT_CLASS \
+    {'O','b','j','e','c','t','C','l','a','s','s',0}
+#define DIRECTORY_ATTR_DOMAIN_NAME \
+    {'D','o','m','a','i','n',0}
+#define DIRECTORY_ATTR_NETBIOS_NAME \
+    {'N','e','t','B','I','O','S','N','a','m','e',0}
+#define DIRECTORY_ATTR_COMMON_NAME \
+    {'C','o','m','m','o','n','N','a','m','e',0}
+#define DIRECTORY_ATTR_SAM_ACCOUNT_NAME \
+    {'S','a','m','A','c','c','o','u','n','t','N','a','m','e',0}
+#define DIRECTORY_ATTR_USER_PRINCIPAL_NAME \
+    {'U','s','e','r','P','r','i','n','c','i','p','a','l','N','a','m','e',0}
+#define DIRECTORY_ATTR_DESCRIPTION \
+    {'D','e','s','c','r','i','p','t','i','o','n',0}
+#define DIRECTORY_ATTR_COMMENT \
+    {'C','o','m','m','e','n','t',0}
+#define DIRECTORY_ATTR_UID \
+    {'U','I','D',0}
+#define DIRECTORY_ATTR_PASSWORD \
+    {'P','a','s','s','w','o','r','d',0}
+#define DIRECTORY_ATTR_ACCOUNT_FLAGS \
+    {'A','c','c','o','u','n','t','F','l','a','g','s',0}
+#define DIRECTORY_ATTR_GECOS \
+    {'G','e','c','o','s',0}
+#define DIRECTORY_ATTR_HOME_DIR \
+    {'H','o','m','e','d','i','r',0}
+#define DIRECTORY_ATTR_HOME_DRIVE \
+    {'H','o','m','e','d','r','i','v','e',0}
+#define DIRECTORY_ATTR_LOGON_SCRIPT \
+    {'L','o','g','o','n','S','c','r','i','p','t',0}
+#define DIRECTORY_ATTR_PROFILE_PATH \
+    {'P','r','o','f','i','l','e','P','a','t','h',0}
+#define DIRECTORY_ATTR_WORKSTATIONS \
+    {'W','o','r','k','s','t','a','t','i','o','n','s',0}
+#define DIRECTORY_ATTR_PARAMETERS \
+    {'P','a','r','a','m','e','t','e','r','s',0}
+#define DIRECTORY_ATTR_SHELL \
+    {'L','o','g','i','n','S','h','e','l','l',0}
+#define DIRECTORY_ATTR_PASSWORD_LAST_SET \
+    {'P','a','s','s','w','o','r','d','L','a','s','t','S','e','t',0}
+#define DIRECTORY_ATTR_ALLOW_PASSWORD_CHANGE \
+    {'A','l','l','o','w','P','a','s','s','w','o','r','d','C','h','a','n','g','e',0}
+#define DIRECTORY_ATTR_FORCE_PASSWORD_CHANGE \
+    {'F','o','r','c','e','P','a','s','s','w','o','r','d','C','h','a','n','g','e',0}
+#define DIRECTORY_ATTR_FULL_NAME \
+    {'F','u','l','l','N','a','m','e',0}
+#define DIRECTORY_ATTR_ACCOUNT_EXPIRY \
+    {'A','c','c','o','u','n','t','E','x','p','i','r','y',0}
+#define DIRECTORY_ATTR_LM_HASH \
+    {'L','M','H','a','s','h',0}
+#define DIRECTORY_ATTR_NT_HASH \
+    {'N','T','H','a','s','h',0}
+#define DIRECTORY_ATTR_PRIMARY_GROUP \
+    {'P','r','i','m','a','r','y','G','r','o','u','p',0}
+#define DIRECTORY_ATTR_GID \
+    {'G','I','D',0}
+#define DIRECTORY_ATTR_COUNTRY_CODE \
+    {'C','o','u','n','t','r','y','C','o','d','e',0}
+#define DIRECTORY_ATTR_CODE_PAGE \
+    {'C','o','d','e','P','a','g','e',0}
+#define DIRECTORY_ATTR_MAX_PWD_AGE \
+    {'M','a','x','P','w','d','A','g','e',0}
+#define DIRECTORY_ATTR_MIN_PWD_AGE \
+    {'M','i','n','P','w','d','A','g','e',0}
+#define DIRECTORY_ATTR_PWD_PROMPT_TIME \
+    {'P','w','d','P','r','o','m','p','t','T','i','m','e',0}
+#define DIRECTORY_ATTR_LAST_LOGON \
+    {'L','a','s','t','L','o','g','o','n',0}
+#define DIRECTORY_ATTR_LAST_LOGOFF \
+    {'L','a','s','t','L','o','g','o','f','f',0}
+#define DIRECTORY_ATTR_LOCKOUT_TIME \
+    {'L','o','c','k','o','u','t','T','i','m','e',0}
+#define DIRECTORY_ATTR_LOGON_COUNT \
+    {'L','o','g','o','n','C','o','u','n','t',0}
+#define DIRECTORY_ATTR_BAD_PASSWORD_COUNT \
+    {'B','a','d','P','w','d','C','o','u','n','t',0}
+#define DIRECTORY_ATTR_LOGON_HOURS \
+    {'L','o','g','o','n','H','o','u','r','s',0}
+#define DIRECTORY_ATTR_ROLE \
+    {'R','o','l','e',0}
+#define DIRECTORY_ATTR_MIN_PWD_LENGTH \
+    {'M','i','n','P','w','d','L','e','n','g','t','h',0}
+#define DIRECTORY_ATTR_PWD_HISTORY_LENGTH \
+    {'P','w','d','H','i','s','t','o','r','y','L','e','n','g','t','h',0}
+#define DIRECTORY_ATTR_PWD_PROPERTIES \
+    {'P','w','d','P','r','o','p','e','r','t','i','e','s',0}
+#define DIRECTORY_ATTR_FORCE_LOGOFF_TIME \
+    {'F','o','r','c','e','L','o','g','o','f','f','T','i','m','e',0}
+#define DIRECTORY_ATTR_PRIMARY_DOMAIN \
+    {'P','r','i','m','a','r','y','D','o','m','a','i','n',0}
+#define DIRECTORY_ATTR_SEQUENCE_NUMBER \
+    {'S','e','q','u','e','n','c','e','N','u','m','b','e','r',0}
+#define DIRECTORY_ATTR_LOCKOUT_DURATION \
+    {'L','o','c','k','o','u','t','D','u','r','a','t','i','o','n',0}
+#define DIRECTORY_ATTR_LOCKOUT_WINDOW \
+    {'L','o','c','k','o','u','t','W','i','n','d','o','w',0}
+#define DIRECTORY_ATTR_LOCKOUT_THRESHOLD \
+    {'L','o','c','k','o','u','t','T','h','r','e','s','h','o','l','d',0}
+#define DIRECTORY_ATTR_CREATED_TIME \
+    {'C','r','e','a','t','e','d','T','i','m','e',0}
+#define DIRECTORY_ATTR_MEMBERS \
+    {'M','e','m','b','e','r','s',0}
+
+
 #define DIRECTORY_FREE_STRING(pszStr) \
     if (pszStr) { \
         DirectoryFreeString(pszStr); \
