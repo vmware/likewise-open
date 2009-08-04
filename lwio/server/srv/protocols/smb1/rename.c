5a3268bad01eb2a8a0b46c0544dd095d3e0187fe
Gerald W. Carter <gcarter@likewiseopen.org>
2009-08-04 14:17:11 +0000

    srv: Use FILE_SHARE_{READ,WRITE,DELETE} fior internal IoCreateFile() calls.
    When deleting or renaming a file, make sure to use all the FILE_SHARE_XXX
    ShareAccess flags to avoid an unnecessary sharing violation.
    
    (lwio: r35979)

--- lwio/server/srv/protocols/smb1/rename.c
+++ lwio/server/srv/protocols/smb1/rename.c
@@ -199,7 +199,7 @@ SrvExecuteRename(
                     DELETE,
                     0,
                     FILE_ATTRIBUTE_NORMAL,
-                    0,
+                    FILE_SHARE_DELETE|FILE_SHARE_READ|FILE_SHARE_WRITE,
                     FILE_OPEN,
                     0,
                     NULL, /* EA Buffer */
