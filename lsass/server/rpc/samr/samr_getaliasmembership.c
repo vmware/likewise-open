206eaa6df5184cc0030816a0439bf87306b754e3
Rafal Szczesniak <rafal@likewise.com>
2009-08-21 08:48:07 +0000

    lsass:rpc: Implement internals of SamrGetAliasMembership server function.
    (lsass: r36493)

--- lsass/server/rpc/samr/samr_getaliasmembership.c
+++ lsass/server/rpc/samr/samr_getaliasmembership.c
@@ -47,6 +47,22 @@
 #include "includes.h"
 
 
+static
+VOID
+FreeSid(
+    PVOID pData,
+    PVOID pUserData
+    );
+
+
+static
+VOID
+SetRidsArray(
+    PVOID pData,
+    PVOID pUserData
+    );
+
+
 NTSTATUS SamrSrvGetAliasMembership(
     /* [in] */ handle_t hBinding,
     /* [in] */ DOMAIN_HANDLE hDomain,
@@ -54,10 +70,224 @@ NTSTATUS SamrSrvGetAliasMembership(
     /* [out] */ Ids *pRids
     )
 {
+    wchar_t wszFilterFmt[] = L"%ws='%ws'";
     NTSTATUS status = STATUS_SUCCESS;
+    PDOMAIN_CONTEXT pDomCtx = NULL;
+    DWORD dwError = 0;
+    DWORD dwDnNum = 0;
+    PWSTR *ppwszDn = NULL;
+    PWSTR pwszSid = NULL;
+    PSID pSid = NULL;
+    DWORD i = 0;
+    DWORD iGroup = 0;
+    PDLINKEDLIST pSidList = NULL;
+    HANDLE hDirectory = NULL;
+    DWORD dwScope = 0;
+    PWSTR pwszBase = NULL;
+    DWORD dwSidStrLen = 0;
+    DWORD dwFilterLen = 0;
+    PWSTR pwszFilter = NULL;
+    WCHAR wszAttrObjectSid[] = DS_ATTR_OBJECT_SID;
+    WCHAR wszAttrDn[] = DS_ATTR_DISTINGUISHED_NAME;
+    PDIRECTORY_ENTRY pEntry = NULL;
+    DWORD dwEntriesNum = 0;
+    PDIRECTORY_ENTRY pLocalGroupEntries = NULL;
+    DWORD dwLocalGroupsNum = 0;
+    PWSTR pwszDn = NULL;
+    DWORD dwSidCount = 0;
+    Ids Rids = {0};
+
+    PWSTR wszMemberAttributes[] = {
+        wszAttrDn,
+        NULL
+    };
+
+    PWSTR wszLocalGroupAttributes[] = {
+        wszAttrObjectSid,
+        NULL
+    };
+
+    pDomCtx    = (PDOMAIN_CONTEXT)hDomain;
+    hDirectory = pDomCtx->pConnCtx->hDirectory;
+    dwDnNum    = pSids->num_sids;
+
+    dwError = LwAllocateMemory(sizeof(PWSTR) * dwDnNum,
+                               OUT_PPVOID(&ppwszDn));
+    BAIL_ON_LSA_ERROR(dwError);
+
+    for (i = 0; i < pSids->num_sids; i++)
+    {
+        status = RtlAllocateWC16StringFromSid(&pwszSid,
+                                              pSids->sids[i].sid);
+        BAIL_ON_NTSTATUS_ERROR(status);
+
+        dwError = LwWc16sLen(pwszSid, &dwSidStrLen);
+        BAIL_ON_LSA_ERROR(dwError);
+
+        dwFilterLen = ((sizeof(wszAttrObjectSid)/sizeof(WCHAR)) - 1) +
+                      dwSidStrLen +
+                      (sizeof(wszFilterFmt)/sizeof(wszFilterFmt[0]));
+
+        dwError = LwAllocateMemory(sizeof(WCHAR) * dwFilterLen,
+                                   OUT_PPVOID(&pwszFilter));
+        BAIL_ON_LSA_ERROR(dwError);
+
+        sw16printfw(pwszFilter, dwFilterLen, wszFilterFmt,
+                    wszAttrObjectSid,
+                    pwszSid);
+
+        dwError = DirectorySearch(hDirectory,
+                                  pwszBase,
+                                  dwScope,
+                                  pwszFilter,
+                                  wszMemberAttributes,
+                                  FALSE,
+                                  &pEntry,
+                                  &dwEntriesNum);
+        BAIL_ON_LSA_ERROR(dwError);
+
+        if (dwEntriesNum > 1)
+        {
+            status = STATUS_INTERNAL_ERROR;
+            BAIL_ON_NTSTATUS_ERROR(status);
+        }
+        else if (dwEntriesNum == 1)
+        {
+            dwError = DirectoryGetEntryAttrValueByName(
+                                      pEntry,
+                                      wszAttrDn,
+                                      DIRECTORY_ATTR_TYPE_UNICODE_STRING,
+                                      &pwszDn);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            if (pwszDn == NULL)
+            {
+                status = STATUS_INTERNAL_ERROR;
+                BAIL_ON_NTSTATUS_ERROR(status);
+            }
+
+            dwError = LwAllocateWc16String(&ppwszDn[i],
+                                           pwszDn);
+            BAIL_ON_LSA_ERROR(status);
+        }
+
+        if (pEntry)
+        {
+            DirectoryFreeEntries(pEntry, dwEntriesNum);
+            pEntry = NULL;
+        }
+
+        LW_SAFE_FREE_MEMORY(pwszFilter);
+        RTL_FREE(&pwszSid);
+
+        pwszFilter = NULL;
+        pwszSid    = NULL;
+    }
+
+    for (i = 0; i < dwDnNum; i++)
+    {
+        pwszDn = ppwszDn[i];
+
+        dwError = DirectoryGetMemberships(
+                                  hDirectory,
+                                  pwszDn,
+                                  wszLocalGroupAttributes,
+                                  &pLocalGroupEntries,
+                                  &dwLocalGroupsNum);
+        BAIL_ON_LSA_ERROR(dwError);
+
+        for (iGroup = 0; iGroup < dwLocalGroupsNum; iGroup++)
+        {
+            dwError = DirectoryGetEntryAttrValueByName(
+                                  pLocalGroupEntries,
+                                  wszAttrObjectSid,
+                                  DIRECTORY_ATTR_TYPE_UNICODE_STRING,
+                                  &pwszSid);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            if (pwszSid == NULL)
+            {
+                status = STATUS_INTERNAL_ERROR;
+                BAIL_ON_NTSTATUS_ERROR(status);
+            }
+
+            status = RtlAllocateSidFromWC16String(&pSid, pwszSid);
+            BAIL_ON_NTSTATUS_ERROR(status);
+
+            if (RtlIsPrefixSid(pDomCtx->pDomainSid, pSid))
+            {
+                dwError = LsaDLinkedListAppend(&pSidList, pSid);
+                BAIL_ON_LSA_ERROR(dwError);
+            }
+        }
+
+        if (pLocalGroupEntries)
+        {
+            DirectoryFreeEntries(pLocalGroupEntries,
+                                 dwLocalGroupsNum);
+        }
+    }
+
+    dwSidCount = LsaDLinkedListLength(pSidList);
+
+    status = SamrSrvAllocateMemory(OUT_PPVOID(&Rids.ids),
+                                   sizeof(Rids.ids[0]) * dwSidCount);
+    BAIL_ON_NTSTATUS_ERROR(status);
+
+    LsaDLinkedListForEach(pSidList, SetRidsArray, (PVOID)&Rids);
+
+cleanup:
+    if (pEntry)
+    {
+        DirectoryFreeEntries(pEntry, dwEntriesNum);
+    }
+
+    if (pLocalGroupEntries)
+    {
+        DirectoryFreeEntries(pLocalGroupEntries,
+                             dwLocalGroupsNum);
+    }
+
+    LW_SAFE_FREE_MEMORY(pwszFilter);
+    RTL_FREE(&pwszSid);
+
+    LsaDLinkedListForEach(pSidList, FreeSid, NULL);
+    LsaDLinkedListFree(pSidList);
 
     return status;
+
+error:
+    goto cleanup;
 }
+
+
+static
+VOID
+FreeSid(
+    PVOID pData,
+    PVOID pUserData
+    )
+{
+    PSID pSid = (PSID)pData;
+    RTL_FREE(&pSid);
+}
+
+
+static
+VOID
+SetRidsArray(
+    PVOID pData,
+    PVOID pUserData
+    )
+{
+    PSID pAliasSid = (PSID)pData;
+    Ids *pRids = (Ids*)pUserData;
+    DWORD i = pRids->count;
+
+    pRids->ids[i] = pAliasSid->SubAuthority[pAliasSid->SubAuthorityCount - 1];
+    pRids->count  = ++i;
+}
+
 
 /*
 local variables:
