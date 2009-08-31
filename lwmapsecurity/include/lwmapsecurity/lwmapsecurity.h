1964b2b339cda3acac259371c1536fde8685bd07
Sriram Nambakam <snambakam@likewise.com>
2009-08-31 17:21:23 +0000

    lwmapsecurity: maintain single ref-counted lw map security context
    (lwmapsecurity: r36796)

--- lwmapsecurity/include/lwmapsecurity/lwmapsecurity.h
+++ lwmapsecurity/include/lwmapsecurity/lwmapsecurity.h
@@ -32,71 +32,81 @@
 typedef struct _LW_MAP_SECURITY_CONTEXT *PLW_MAP_SECURITY_CONTEXT;
 
 NTSTATUS
-LwMapSecurityCreateContext(
-    OUT PLW_MAP_SECURITY_CONTEXT* Context
+LwMapSecurityInit(
+    VOID
     );
 
-VOID
-LwMapSecurityFreeContext(
-    IN OUT PLW_MAP_SECURITY_CONTEXT* Context
+NTSTATUS
+LwMapSecurityCreateContext(
+    OUT PLW_MAP_SECURITY_CONTEXT* ppContext
     );
 
 NTSTATUS
 LwMapSecurityGetIdFromSid(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PBOOLEAN IsUser,
-    OUT PULONG Id,
-    IN PSID Sid
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PBOOLEAN                 IsUser,
+    OUT PULONG                   Id,
+    IN  PSID                     pSid
     );
 
 NTSTATUS
 LwMapSecurityGetSidFromId(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PSID* Sid,
-    IN BOOLEAN IsUser,
-    IN ULONG Id
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PSID*                    pSid,
+    IN  BOOLEAN                  IsUser,
+    IN  ULONG                    Id
     );
 
 VOID
 LwMapSecurityFreeSid(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    IN OUT PSID* Sid
+    IN     PLW_MAP_SECURITY_CONTEXT pContext,
+    IN OUT PSID*                    ppSid
     );
 
 NTSTATUS
 LwMapSecurityCreateAccessTokenFromUidGid(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PACCESS_TOKEN* AccessToken,
-    IN ULONG Uid,
-    IN ULONG Gid
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PACCESS_TOKEN*           ppAccessToken,
+    IN  ULONG                    Uid,
+    IN  ULONG                    Gid
     );
 
 NTSTATUS
 LwMapSecurityCreateAccessTokenFromUnicodeStringUsername(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PACCESS_TOKEN* AccessToken,
-    IN PUNICODE_STRING Username
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PACCESS_TOKEN*           ppAccessToken,
+    IN  PUNICODE_STRING          Username
     );
 
 NTSTATUS
 LwMapSecurityCreateAccessTokenFromAnsiStringUsername(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PACCESS_TOKEN* AccessToken,
-    IN PANSI_STRING Username
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PACCESS_TOKEN*           ppAccessToken,
+    IN  PANSI_STRING             Username
     );
 
 NTSTATUS
 LwMapSecurityCreateAccessTokenFromWC16StringUsername(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PACCESS_TOKEN* AccessToken,
-    IN PCWSTR Username
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PACCESS_TOKEN*           ppAccessToken,
+    IN  PCWSTR                   Username
     );
 
 NTSTATUS
 LwMapSecurityCreateAccessTokenFromCStringUsername(
-    IN PLW_MAP_SECURITY_CONTEXT Context,
-    OUT PACCESS_TOKEN* AccessToken,
-    IN PCSTR Username
+    IN  PLW_MAP_SECURITY_CONTEXT pContext,
+    OUT PACCESS_TOKEN*           ppAccessToken,
+    IN  PCSTR                    Username
+    );
+
+VOID
+LwMapSecurityFreeContext(
+    IN OUT PLW_MAP_SECURITY_CONTEXT* ppContext
+    );
+
+NTSTATUS
+LwMapSecurityShutdown(
+    VOID
     );
 
 #endif /* __LW_MAP_SECURITY_H__ */
