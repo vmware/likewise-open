65c33f275dfd74034a9b1143b129797d1a51a11c
Arlene Berry <aberry@likewise.com>
2009-08-31 17:51:23 +0000

    Fix build break due to unsupported find option on some platforms.
    (krb5: r36799)

--- krb5/src/autogen.sh
+++ krb5/src/autogen.sh
@@ -2,9 +2,4 @@
 
 top=`pwd`
 
-for configurein in `find . -path ./appl -prune -o -name configure.in -print` ; do
-    pushd `dirname $configurein` > /dev/null
-    echo "Running autoconf in `dirname ${configurein}`/"
-    autoconf -I "$top"
-    popd > /dev/null
-done
+autoconf -I "$top"
