cbd3c1d42c5447e150318e473c251b7e7500c6a2
Rafal Szczesniak <rafal@likewise.com>
2009-09-02 13:52:47 +0000

    librpc: Include lwadvapi error handling headers in NetAPI error header now.
    (librpc: r36891)

--- librpc/include/lwrpc/LMErr.h
+++ librpc/include/lwrpc/LMErr.h
@@ -1,6 +1,6 @@
 /* Editor Settings: expandtabs and use 4 spaces for indentation
  * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
- * -*- mode: c, c-basic-offset: 4 -*- */
+ */
 
 /*
  * Copyright Likewise Software    2004-2008
@@ -29,4 +29,14 @@
  */
 
 #include <lw/ntstatus.h>
-#include <lwrpc/winerror.h>
+#include <winerror.h>
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
