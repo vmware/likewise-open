905e29ae4c0d4143ca23f5429572cd35bfded8d2
Rafal Szczesniak <rafal@likewise.com>
2009-09-04 13:52:45 +0000

    lsass:rpc: Use WINERR codes for "member in group" and "member not in group" type of errors instead of LW_ERROR.
    (lsass: r36976)

--- lsass/server/rpc/samr/samr_deletealiasmember.c
+++ lsass/server/rpc/samr/samr_deletealiasmember.c
@@ -135,20 +135,7 @@ SamrSrvDeleteAliasMember(
     dwError = DirectoryRemoveFromGroup(hDirectory,
                                        pwszGroupDn,
                                        pEntry);
-
-    if (dwError == LW_ERROR_MEMBER_NOT_IN_LOCAL_GROUP)
-    {
-        ntStatus = STATUS_MEMBER_NOT_IN_GROUP;
-    }
-    else if (dwError == ERROR_SUCCESS)
-    {
-        ntStatus = STATUS_SUCCESS;
-    }
-    else
-    {
-        ntStatus = STATUS_INTERNAL_ERROR;
-    }
-    BAIL_ON_NTSTATUS_ERROR(ntStatus);
+    BAIL_ON_LSA_ERROR(dwError);
 
 cleanup:
     if (pEntry)
@@ -158,6 +145,12 @@ cleanup:
 
     LW_SAFE_FREE_MEMORY(pwszFilter);
     RTL_FREE(&pwszSid);
+
+    if (ntStatus == STATUS_SUCCESS &&
+        dwError != ERROR_SUCCESS)
+    {
+        ntStatus = LwWin32ErrorToNtStatus(dwError);
+    }
 
     return ntStatus;
 
