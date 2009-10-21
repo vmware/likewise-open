08a46174748ebac328731b3bd47242314fc67270
Gerald W. Carter <gcarter@likewiseopen.org>
2009-10-21 19:46:59 +0000

    srv: BUG 9007 - Implement FileFullDirInformation for SMBv1's FindFirst.
    Fixes 'dir' from a Win7 client.
    
    (lwio: r38292)

--- lwio/server/include/smbwire.h
+++ lwio/server/include/smbwire.h
@@ -1413,6 +1413,23 @@ typedef struct _SMB_FIND_FILE_BOTH_DIRECTORY_INFO_HEADER
 } __attribute__((__packed__)) SMB_FIND_FILE_BOTH_DIRECTORY_INFO_HEADER,
                              *PSMB_FIND_FILE_BOTH_DIRECTORY_INFO_HEADER;
 
+typedef struct _SMB_FIND_FILE_FULL_DIRECTORY_INFO_HEADER
+{
+    ULONG     NextEntryOffset;
+    ULONG     FileIndex;
+    LONG64    CreationTime;
+    LONG64    LastAccessTime;
+    LONG64    LastWriteTime;
+    LONG64    ChangeTime;
+    LONG64    EndOfFile;
+    LONG64    AllocationSize;
+    FILE_ATTRIBUTES FileAttributes;
+    ULONG     FileNameLength;
+    ULONG     EaSize;
+    WCHAR     FileName[];
+} __attribute__((__packed__)) SMB_FIND_FILE_FULL_DIRECTORY_INFO_HEADER,
+                             *PSMB_FIND_FILE_FULL_DIRECTORY_INFO_HEADER;
+
 typedef struct _SMB_FIND_FILE_NAMES_INFO_HEADER
 {
     ULONG     NextEntryOffset;
