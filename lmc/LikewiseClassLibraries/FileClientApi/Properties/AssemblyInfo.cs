2096e48e831e9dc5d826386ac330b341c732eba5
Contract <contractor@likewise.com>
2009-10-27 14:02:46 +0000

    Trying to fix the build break for FileBrowser plugin
    (lmc: r38486)

--- lmc/LikewiseClassLibraries/FileClientApi/Properties/AssemblyInfo.cs
+++ lmc/LikewiseClassLibraries/FileClientApi/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+#if VSTUDIO_BUILD
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -14,14 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aa71095b-c014-4dd4-b799-db5800ef518d")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -33,3 +26,13 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa71095b-c014-4dd4-b799-db5800ef518d")]
+
