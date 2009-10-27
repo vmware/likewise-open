f1b56325b322fe3de94318b2398f90cda7fa2c18
Gerald W. Carter <gcarter@likewiseopen.org>
2009-10-27 16:39:02 +0000

    srv: Add SRV_HOST_INFO->bIsJoined to save the state of the machine in AD.
    Store and empty string ("") for the domain name if LWNetGetCurrentDomain
    returns ERROR_NOT_JOINED.  Remember this in the HostInfo->bIsjOIned flag.
    
    (lwio: r38496)

--- lwio/server/srv/utils/hostinfo.c
+++ lwio/server/srv/utils/hostinfo.c
@@ -1,3 +1,48 @@
+/*
+ * Copyright Likewise Software    2004-2009
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.  You should have received a copy of the GNU General
+ * Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * LIKEWISE SOFTWARE MAKES THIS SOFTWARE AVAILABLE UNDER OTHER LICENSING
+ * TERMS AS WELL.  IF YOU HAVE ENTERED INTO A SEPARATE LICENSE AGREEMENT
+ * WITH LIKEWISE SOFTWARE, THEN YOU MAY ELECT TO USE THE SOFTWARE UNDER THE
+ * TERMS OF THAT SOFTWARE LICENSE AGREEMENT INSTEAD OF THE TERMS OF THE GNU
+ * GENERAL PUBLIC LICENSE, NOTWITHSTANDING THE ABOVE NOTICE.  IF YOU
+ * HAVE QUESTIONS, OR WISH TO REQUEST A COPY OF THE ALTERNATE LICENSING
+ * TERMS OFFERED BY LIKEWISE SOFTWARE, PLEASE CONTACT LIKEWISE SOFTWARE AT
+ * license@likewisesoftware.com
+ */
+
+/*
+ * Copyright (C) Likewise Software. All rights reserved.
+ *
+ * Module Name:
+ *
+ *        hostinfo.c
+ *
+ * Abstract:
+ *
+ *        Likewise Input Output (LWIO) - SRV
+ *
+ *        Utilities
+ *
+ *        Producer Consumer Queue
+ *
+ * Authors: Sriram Nambakam (snambakam@likewise.com)
+ *
+ */
+
 #include "includes.h"
 
 static
@@ -13,6 +58,7 @@ SrvAcquireHostInfo(
     )
 {
     NTSTATUS ntStatus = 0;
+    DWORD dwError = 0;
     PSTR pszDomain = NULL;
     PSRV_HOST_INFO pHostInfo = NULL;
     PSRV_HOST_INFO pNewHostInfo = NULL;
@@ -29,16 +75,25 @@ SrvAcquireHostInfo(
         pthread_rwlock_init(&pHostInfo->mutex, NULL);
         pHostInfo->pMutex = &pHostInfo->mutex;
 
-        ntStatus = LWNetGetCurrentDomain(&pszDomain);
+        dwError = LWNetGetCurrentDomain(&pszDomain);
+        switch (dwError)
+        {
+        case ERROR_SUCCESS:
+            pHostInfo->bIsJoined = TRUE;
+            ntStatus = SMBAllocateString(pszDomain, &pHostInfo->pszDomain);
+            break;
+        case ERROR_NOT_JOINED:
+            pHostInfo->bIsJoined = FALSE;
+            ntStatus = SMBAllocateString("", &pHostInfo->pszDomain);
+            break;
+        default:
+            // Need proper WinError -> NTSTATUS mapping here
+            ntStatus = STATUS_OBJECT_NAME_NOT_FOUND;
+            break;
+        }
         BAIL_ON_NT_STATUS(ntStatus);
 
-        ntStatus = LwioGetHostInfo(
-                        &pHostInfo->pszHostname);
-        BAIL_ON_NT_STATUS(ntStatus);
-
-        ntStatus = SMBAllocateString(
-                        pszDomain,
-                        &pHostInfo->pszDomain);
+        ntStatus = LwioGetHostInfo(&pHostInfo->pszHostname);
         BAIL_ON_NT_STATUS(ntStatus);
 
         pNewHostInfo = pHostInfo;
@@ -99,3 +154,13 @@ SrvFreeHostInfo(
 
     SrvFreeMemory(pHostinfo);
 }
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
