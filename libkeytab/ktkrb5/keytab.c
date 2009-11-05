4cc97be78d4e433e7a0c92cf32e19e813cdaf6e8
Oem <oem@likewise.com>
2009-11-05 18:50:02 +0000

    libkeytab: fix memory leak
    Reviewed by: Sriram Nambakam
    
    (libkeytab: r38715)

--- libkeytab/ktkrb5/keytab.c
+++ libkeytab/ktkrb5/keytab.c
@@ -126,6 +126,8 @@ error:
 
     KT_SAFE_FREE_STRING(pszDefName);
     KT_SAFE_FREE_STRING(pszKtName);
+    KT_SAFE_FREE_STRING(pszKtFilename);
+    KT_SAFE_FREE_STRING(pszDefName);
 
     return dwError;
 }
