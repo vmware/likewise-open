efa611b70376b9ab11bb7c250a87c53eca1a980b
Adam Bernstein <abernstein@likewise.com>
2009-11-24 23:49:26 +0000

    Revert change 39327. This breaks import.
    (lwreg: r39349)

--- lwreg/parse/reglex.c
+++ lwreg/parse/reglex.c
@@ -892,7 +892,6 @@ RegLexGetToken(
         lexHandle->isToken = FALSE;
         lexHandle->curToken.token = REGLEX_FIRST;
         lexHandle->curToken.valueCursor = 0;
-        lexHandle->curToken.pszValue[0] = '\0';
     }
 
     /* Return pushed back token, if one is present */
