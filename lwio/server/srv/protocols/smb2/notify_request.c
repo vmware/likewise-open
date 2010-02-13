b723da4003c740f1cad2d76aa7ea4824e03f7cc9
Sriram Nambakam <snambakam@likewise.com>
2010-02-13 17:32:13 +0000

    lwio:srv:smb2: A change notify request must be the only message in a request, or the last mesage in a chain - otherwise, return STATUS_INTERNAL_ERROR for it.
    (lwio: r41072)

--- lwio/server/srv/protocols/smb2/notify_request.c
+++ lwio/server/srv/protocols/smb2/notify_request.c
@@ -175,6 +175,14 @@ SrvProcessNotify_SMB_V2(
                  BAIL_ON_NT_STATUS(ntStatus);
              }
 
+             // A change notify request can occur as the only message in
+             // a request, or the last in a chained request
+             if (pCtxSmb2->iMsg != (pCtxSmb2->ulNumRequests - 1))
+             {
+                 ntStatus = STATUS_INTERNAL_ERROR;
+                 BAIL_ON_NT_STATUS(ntStatus);
+             }
+
              ntStatus = SrvAsyncBuildUniqueId(
                              pExecContext,
                              &pNotifyRequestState->ullAsyncId);
