6e70b4bf90b968f2ea55b11d86da4191cb91dbd9
Zach Loafman <zachary.loafman@isilon.com>
2010-02-26 16:31:20 +0000

    lwio:srv:smb2: Fix copy/paste error in notify_request.c
    (lwio: r41520)

--- lwio/server/srv/protocols/smb2/notify_request.c
+++ lwio/server/srv/protocols/smb2/notify_request.c
@@ -175,14 +175,12 @@ SrvProcessNotify_SMB_V2(
                  BAIL_ON_NT_STATUS(ntStatus);
              }
 
-#if 0
-             if (pNTTransactState->pRequestHeader->ulMaxParameterCount >
+             if (pNotifyRequestState->pRequestHeader->ulOutputBufferLength >
                  SMB_CN_MAX_BUFFER_SIZE)
              {
                  ntStatus = STATUS_INVALID_BUFFER_SIZE;
                  BAIL_ON_NT_STATUS(ntStatus);
              }
-#endif
 
              // A change notify request can occur as the only message in
              // a request, or the last in a chained request
