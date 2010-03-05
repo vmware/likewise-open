1ddadadd04fd36017b5c542d14e9c73015b67235
Gerald W. Carter <gcarter@likewiseopen.org>
2010-03-05 15:32:25 +0000

    MERGE: -c 41580 /trunk/Sapphire -> /branches/lwidentity-5.4
    lwio:srv:smb2: Do not use the session id in SMB2-CANCEL.
    Part of fix for bug#10092
    
    (lwio: r41806)

--- lwio/server/srv/protocols/smb2/notify_request.c
+++ lwio/server/srv/protocols/smb2/notify_request.c
@@ -464,18 +464,10 @@ SrvCancelChangeNotify_SMB_V2(
     PSRV_EXEC_CONTEXT_SMB_V2   pCtxSmb2     = pCtxProtocol->pSmb2Context;
     ULONG                      iMsg         = pCtxSmb2->iMsg;
     PSRV_MESSAGE_SMB_V2        pSmbRequest  = &pCtxSmb2->pRequests[iMsg];
-    PLWIO_SRV_SESSION_2        pSession     = NULL;
     BOOLEAN                    bInLock      = FALSE;
     PLWIO_ASYNC_STATE          pAsyncState  = NULL;
     ULONG64                    ullAsyncId   = 0LL;
     PSRV_NOTIFY_STATE_SMB_V2   pNotifyState = NULL;
-
-    ntStatus = SrvConnection2FindSession_SMB_V2(
-                            pCtxSmb2,
-                            pConnection,
-                            pSmbRequest->pHeader->ullSessionId,
-                            &pSession);
-    BAIL_ON_NT_STATUS(ntStatus);
 
     ntStatus = SMB2GetAsyncId(pSmbRequest->pHeader, &ullAsyncId);
     BAIL_ON_NT_STATUS(ntStatus);
@@ -502,11 +494,6 @@ cleanup:
     if (pAsyncState)
     {
         SrvAsyncStateRelease(pAsyncState);
-    }
-
-    if (pSession)
-    {
-        SrvSession2Release(pSession);
     }
 
     return ntStatus;
