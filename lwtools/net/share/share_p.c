2d8e784ed015ea10bb8c714d4cf9c65c8a2b7ede
Wei Fu <wfu@likewise.com>
2010-03-22 22:34:47 +0000

    lwtools - adding "./lwnet share add sharename=share_path".
    (lwtools: r42393)

--- lwtools/net/share/share_p.c
+++ lwtools/net/share/share_p.c
@@ -49,6 +49,91 @@
 
 NET_SHARE_STATE gState = {0};
 
+static
+DWORD
+MapNameToSid(
+    HANDLE hLsa,
+    PCWSTR pwszName,
+    PSID* ppSid
+    );
+
+static
+DWORD
+MapBuiltinNameToSid(
+    PSID *ppSid,
+    PCWSTR pwszName
+    );
+
+static
+DWORD
+ConstructSecurityDescriptor(
+    DWORD dwAllowUserCount,
+    PWSTR* ppwszAllowUsers,
+    DWORD dwDenyUserCount,
+    PWSTR* ppwszDenyUsers,
+    BOOLEAN bReadOnly,
+    PSECURITY_DESCRIPTOR_RELATIVE* ppRelative,
+    PDWORD pdwRelativeSize
+    );
+
+
+
+DWORD
+LwUtilNetShareAdd(
+	NET_SHARE_ADD_INFO_PARAMS ShareAddInfo
+    )
+{
+    static const DWORD dwLevel = 502;
+
+    DWORD dwError = 0;
+    SHARE_INFO_502 shareInfo = {0};
+    DWORD dwParmErr = 0;
+    PSECURITY_DESCRIPTOR_RELATIVE pSecDesc = NULL;
+    DWORD dwSecDescSize = 0;
+
+
+    dwError = LwMbsToWc16s(ShareAddInfo.pszPath, &gState.pwszPath);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwMbsToWc16s(ShareAddInfo.pszShareName, &gState.pwszName);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = ConstructSecurityDescriptor(
+        gState.dwAllowUserCount,
+        gState.ppwszAllowUsers,
+        gState.dwDenyUserCount,
+        gState.ppwszDenyUsers,
+        gState.bReadOnly && !gState.bReadWrite,
+        &pSecDesc,
+        &dwSecDescSize);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    shareInfo.shi502_type = 0; // SHARE_SERVICE_DISK_SHARE
+    shareInfo.shi502_netname = gState.pwszName ? gState.pwszName : gState.pwszTarget;
+    shareInfo.shi502_remark = gState.pwszComment;
+    shareInfo.shi502_path = gState.pwszPath;
+    shareInfo.shi502_reserved = dwSecDescSize;
+    shareInfo.shi502_security_descriptor = (PBYTE) pSecDesc;
+
+    dwError = NetShareAddW(
+        gState.pwszServerName,
+        dwLevel,
+        (PBYTE)&shareInfo,
+        &dwParmErr);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+cleanup:
+
+    LW_SAFE_FREE_MEMORY(pSecDesc);
+
+    return dwError;
+
+error:
+
+    goto cleanup;
+}
+
+
 DWORD
 LwUtilNetShareEnum(
     VOID
@@ -239,12 +324,289 @@ error:
     goto cleanup;
 }
 
-
+static
 DWORD
-LwUtilNetShareAdd(
-	NET_SHARE_ADD_INFO_PARAMS ShareAddInfo
+MapNameToSid(
+    HANDLE hLsa,
+    PCWSTR pwszName,
+    PSID* ppSid
     )
 {
-    return ERROR_CALL_NOT_IMPLEMENTED;
+    DWORD dwError = 0;
+    PSTR pszName = NULL;
+    LSA_QUERY_LIST QueryList;
+    PLSA_SECURITY_OBJECT* ppObjects = NULL;
+
+    dwError = LwWc16sToMbs(pwszName, &pszName);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    QueryList.ppszStrings = (PCSTR*) &pszName;
+
+    dwError = LsaFindObjects(
+        hLsa,
+        NULL,
+        0,
+        LSA_OBJECT_TYPE_UNDEFINED,
+        LSA_QUERY_TYPE_BY_NAME,
+        1,
+        QueryList,
+        &ppObjects);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    if (ppObjects[0] == NULL)
+    {
+        dwError = LW_ERROR_NO_SUCH_OBJECT;
+        BAIL_ON_LWUTIL_ERROR(dwError);
+    }
+
+    dwError = LwNtStatusToWin32Error(
+        RtlAllocateSidFromCString(ppSid, ppObjects[0]->pszObjectSid));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+cleanup:
+
+    LsaFreeSecurityObjectList(1, ppObjects);
+
+    LW_SAFE_FREE_STRING(pszName);
+
+    return dwError;
+
+error:
+
+    *ppSid = NULL;
+
+    goto cleanup;
 }
 
+static
+DWORD
+MapBuiltinNameToSid(
+    PSID *ppSid,
+    PCWSTR pwszName
+    )
+{
+    DWORD dwError = 0;
+    union
+    {
+        SID sid;
+        BYTE buffer[SID_MAX_SIZE];
+    } Sid;
+    ULONG SidSize = sizeof(Sid.buffer);
+    PWSTR pwszEveryone = NULL;
+
+    dwError = LwNtStatusToWin32Error(
+                  RtlWC16StringAllocateFromCString(
+                      &pwszEveryone,
+                      "Everyone"));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+
+    if (LwRtlWC16StringIsEqual(pwszName, pwszEveryone, FALSE))
+    {
+        dwError = LwNtStatusToWin32Error(
+                      RtlCreateWellKnownSid(
+                          WinWorldSid,
+                          NULL,
+                          &Sid.sid,
+                          &SidSize));
+    }
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+                  RtlDuplicateSid(ppSid, &Sid.sid));
+
+cleanup:
+    LwRtlWC16StringFree(&pwszEveryone);
+
+    return dwError;
+
+error:
+    goto cleanup;
+}
+
+static
+DWORD
+ConstructSecurityDescriptor(
+    DWORD dwAllowUserCount,
+    PWSTR* ppwszAllowUsers,
+    DWORD dwDenyUserCount,
+    PWSTR* ppwszDenyUsers,
+    BOOLEAN bReadOnly,
+    PSECURITY_DESCRIPTOR_RELATIVE* ppRelative,
+    PDWORD pdwRelativeSize
+    )
+{
+    DWORD dwError = 0;
+    PSECURITY_DESCRIPTOR_ABSOLUTE pAbsolute = NULL;
+    PSECURITY_DESCRIPTOR_RELATIVE pRelative = NULL;
+    union
+    {
+        SID sid;
+        BYTE buffer[SID_MAX_SIZE];
+    } Owner;
+    union
+    {
+        SID sid;
+        BYTE buffer[SID_MAX_SIZE];
+    } Group;
+    ULONG OwnerSidSize = sizeof(Owner.buffer);
+    ULONG GroupSidSize = sizeof(Group.buffer);
+    DWORD dwDaclSize = 0;
+    PACL pDacl = NULL;
+    DWORD dwIndex = 0;
+    PSID pSid = NULL;
+    ULONG ulRelativeSize = 0;
+    HANDLE hLsa = NULL;
+    ACCESS_MASK mask = bReadOnly ?
+                       (FILE_GENERIC_READ|FILE_GENERIC_EXECUTE) :
+                       FILE_ALL_ACCESS;
+
+    dwError = LsaOpenServer(&hLsa);
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlCreateWellKnownSid(
+            WinBuiltinAdministratorsSid,
+            NULL,
+            &Owner.sid,
+            &OwnerSidSize));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlCreateWellKnownSid(
+            WinBuiltinPowerUsersSid,
+            NULL,
+            &Group.sid,
+            &GroupSidSize));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwDaclSize = ACL_HEADER_SIZE +
+        dwAllowUserCount * (sizeof(ACCESS_ALLOWED_ACE) + SID_MAX_SIZE) +
+        dwDenyUserCount * (sizeof(ACCESS_DENIED_ACE) + SID_MAX_SIZE) +
+        RtlLengthSid(&Owner.sid) + RtlLengthSid(&Group.sid);
+
+    dwError = LwAllocateMemory(
+        dwDaclSize,
+        OUT_PPVOID(&pDacl));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlCreateAcl(pDacl, dwDaclSize, ACL_REVISION));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    for (dwIndex = 0; dwIndex < dwDenyUserCount; dwIndex++)
+    {
+        dwError = MapNameToSid(hLsa, ppwszDenyUsers[dwIndex], &pSid);
+        if (dwError != LW_ERROR_SUCCESS)
+        {
+            dwError = MapBuiltinNameToSid(&pSid, ppwszDenyUsers[dwIndex]);
+        }
+
+        BAIL_ON_LWUTIL_ERROR(dwError);
+
+        dwError = LwNtStatusToWin32Error(
+            RtlAddAccessDeniedAceEx(
+                pDacl,
+                ACL_REVISION,
+                0,
+                FILE_ALL_ACCESS,
+                pSid));
+        BAIL_ON_LWUTIL_ERROR(dwError);
+
+        RTL_FREE(&pSid);
+    }
+
+   for (dwIndex = 0; dwIndex < dwAllowUserCount; dwIndex++)
+    {
+        dwError = MapNameToSid(hLsa, ppwszAllowUsers[dwIndex], &pSid);
+        if (dwError != LW_ERROR_SUCCESS)
+        {
+            dwError = MapBuiltinNameToSid(&pSid, ppwszAllowUsers[dwIndex]);
+        }
+        BAIL_ON_LWUTIL_ERROR(dwError);
+
+        dwError = LwNtStatusToWin32Error(
+            RtlAddAccessAllowedAceEx(
+                pDacl,
+                ACL_REVISION,
+                0,
+                mask,
+                pSid));
+        BAIL_ON_LWUTIL_ERROR(dwError);
+
+        RTL_FREE(&pSid);
+    }
+
+    dwError = LwAllocateMemory(
+        SECURITY_DESCRIPTOR_ABSOLUTE_MIN_SIZE,
+        OUT_PPVOID(&pAbsolute));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlCreateSecurityDescriptorAbsolute(
+            pAbsolute,
+            SECURITY_DESCRIPTOR_REVISION));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlSetOwnerSecurityDescriptor(
+            pAbsolute,
+            &Owner.sid,
+            FALSE));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlSetGroupSecurityDescriptor(
+            pAbsolute,
+            &Group.sid,
+            FALSE));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlSetDaclSecurityDescriptor(
+            pAbsolute,
+            TRUE,
+            pDacl,
+            FALSE));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    RtlAbsoluteToSelfRelativeSD(
+        pAbsolute,
+        NULL,
+        &ulRelativeSize);
+
+    dwError = LwAllocateMemory(ulRelativeSize, OUT_PPVOID(&pRelative));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    dwError = LwNtStatusToWin32Error(
+        RtlAbsoluteToSelfRelativeSD(
+            pAbsolute,
+            pRelative,
+            &ulRelativeSize));
+    BAIL_ON_LWUTIL_ERROR(dwError);
+
+    *ppRelative = pRelative;
+    *pdwRelativeSize = ulRelativeSize;
+
+cleanup:
+
+    if (hLsa)
+    {
+        LsaCloseServer(hLsa);
+    }
+
+    RTL_FREE(&pSid);
+    LW_SAFE_FREE_MEMORY(pDacl);
+    LW_SAFE_FREE_MEMORY(pAbsolute);
+
+    return dwError;
+
+error:
+
+    *ppRelative = NULL;
+    *pdwRelativeSize = 0;
+
+    LW_SAFE_FREE_MEMORY(pRelative);
+
+    goto cleanup;
+}
