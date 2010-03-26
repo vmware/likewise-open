f65c91533c95288eb4a4514af764c70961c47865
Kyle Stemen <kstemen@likewise.com>
2010-03-26 16:32:04 +0000

    MERGE: -c 42171 /branches/lwidentity-6.0 -> /trunk/Sapphire
    Fix bug 10086 - domain join fails with unclear error message
    
    When the OU path is not found, lsassd was crashing due to null dereference in librpc. After lsass returned a good error code, the error mapping code in domainjoin was removed.
    
    This revision fixes the crash and readds the error code mappings.
    
    (domainjoin: r42570)

--- domainjoin/libdomainjoin/src/djauthinfo.c
+++ domainjoin/libdomainjoin/src/djauthinfo.c
@@ -1103,6 +1103,7 @@ void DJCreateComputerAccount(
     PSTR dnsDomain = NULL;
     CHAR krb5ConfEnv[256];
     DWORD dwFlags = 0;
+    DWORD dwError = 0;
 
     PSTR likewiseVersion = NULL;
     PSTR likewiseBuild = NULL;
@@ -1176,18 +1177,34 @@ void DJCreateComputerAccount(
 
     LW_CLEANUP_LSERR(exc, LsaOpenServer(&lsa));
 
-    LW_CLEANUP_LSERR(exc, LsaAdJoinDomain(
-                         lsa,
-                         options->computerName,
-                         dnsDomain,
-                         options->domainName,
-                         options->ouName,
-                         options->username,
-                         options->password,
-                         osName,
-                         distro.version,
-                         likewiseOSServicePack,
-                         dwFlags));
+    dwError = LsaAdJoinDomain(
+                 lsa,
+                 options->computerName,
+                 dnsDomain,
+                 options->domainName,
+                 options->ouName,
+                 options->username,
+                 options->password,
+                 osName,
+                 distro.version,
+                 likewiseOSServicePack,
+                 dwFlags);
+    if (dwError)
+    {
+        switch(dwError)
+        {
+            case ERROR_FILE_NOT_FOUND:
+                LW_RAISE_EX(exc, CENTERROR_DOMAINJOIN_INVALID_OU, "Lsass Error", "The OU is invalid.");
+                break;
+            case ERROR_INVALID_PARAMETER:
+                LW_RAISE_EX(exc, CENTERROR_DOMAINJOIN_INVALID_FORMAT, "Lsass Error", "The OU format is invalid.");
+                break;
+            default:
+                LW_RAISE_LSERR(exc, dwError);
+                break;
+        }
+        goto cleanup;
+    }
 
     LW_TRY(exc, DJGuessShortDomainName(
                                  options->domainName,
