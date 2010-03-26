644dd9d8f0533bdd068f784bdde0a9619baa30d5
Kyle Stemen <kstemen@likewise.com>
2010-03-26 00:46:29 +0000

    MERGE: -c 42540 /branches/lwidentity-5.3 -> /trunk/Sapphire
    Fix bug 9105 - pam is misconfigured by domainjoin for OpenSuse 11, SELS 9.x
    
    On SuSE platforms, try_first_pass was used even though pam_unix2 does not understand it. The fix is to use use_first_pass instead.
    
    (domainjoin: r42542)

--- domainjoin/libdomainjoin/src/djpamconf.c
+++ domainjoin/libdomainjoin/src/djpamconf.c
@@ -1531,6 +1531,12 @@ static BOOLEAN PamModuleUnderstandsTryFirstPass( const char * phase, const char 
     if(!strcmp(buffer, "pam_unix") && CTStrStartsWith(moduleBasename, "lib"))
         return FALSE;
 
+    /* pam_unix2 on OpenSuse 11, and SLES 9.x is known to not support this
+     * option. The man page also says it is not supported. No distros that use
+     * pam_unix2 are currently known to suport it. */
+    if (!strcmp(buffer, "pam_unix2"))
+        return FALSE;
+
     return TRUE;
 }
 
