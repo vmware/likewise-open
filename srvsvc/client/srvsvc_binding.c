6675b38f487b55ae522d22b5566312571275fcd8
Arlene Berry <aberry@likewise.com>
2010-07-13 16:45:22 +0000

    MERGE: -c 46706 /trunk/Platform -> /branches/lwidentity-5.4
    Make alter_context to an schannel security context work.
    Remove schannel transport info flag hack so that DCE/RPC
    
    Required lots of manual merging.
    CR:  bkoropoff
    
    (srvsvc: r47330)

--- srvsvc/client/srvsvc_binding.c
+++ srvsvc/client/srvsvc_binding.c
@@ -153,7 +153,6 @@ InitSrvSvcBindingFull(
     {
         rpc_smb_transport_info_from_lwio_creds(
             pCreds,
-            FALSE,
             &hInfo,
             &rpcStatus);
         BAIL_ON_RPC_STATUS(rpcStatus);
