3c01181c42f1d1512fd8f9e248f069dc66b8f03e
Gerald W. Carter <gcarter@likewiseopen.org>
2011-01-18 04:26:16 +0000

    MERGE: -c 52540 /trunk/Platform -> /branches/Platform-CIFS-6.0
    lwbase:threadpool: fix memory leak on task group creation failure
    
    Close CID #12528
    
    (lwbase: r54368)

--- lwbase/src/threadpool-epoll.c
+++ lwbase/src/threadpool-epoll.c
@@ -751,20 +751,29 @@ LwRtlCreateTaskGroup(
     status = LW_RTL_ALLOCATE_AUTO(&pGroup);
     GOTO_ERROR_ON_STATUS(status);
 
+    pGroup->pPool = pPool;
+    RingInit(&pGroup->Tasks);
+
     status = LwErrnoToNtStatus(pthread_mutex_init(&pGroup->Lock, NULL));
     GOTO_ERROR_ON_STATUS(status);
+    pGroup->bLockInit = TRUE;
 
     status = LwErrnoToNtStatus(pthread_cond_init(&pGroup->Event, NULL));
     GOTO_ERROR_ON_STATUS(status);
-
-    RingInit(&pGroup->Tasks);
-    pGroup->pPool = pPool;
+    pGroup->bEventInit = TRUE;
 
     *ppGroup = pGroup;
 
-error:
+cleanup:
 
     return status;
+
+error:
+
+    LwRtlFreeTaskGroup(&pGroup);
+    *ppGroup = NULL;
+
+    goto cleanup;
 }
 
 VOID
@@ -803,8 +812,16 @@ LwRtlFreeTaskGroup(
 
     if (pGroup)
     {
-        pthread_mutex_destroy(&pGroup->Lock);
-        pthread_cond_destroy(&pGroup->Event);
+        if (pGroup->bLockInit)
+        {
+            pthread_mutex_destroy(&pGroup->Lock);
+        }
+
+        if (pGroup->bEventInit)
+        {
+            pthread_cond_destroy(&pGroup->Event);
+        }
+
         RtlMemoryFree(pGroup);
 
         *ppGroup = NULL;
