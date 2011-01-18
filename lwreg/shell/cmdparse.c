737a7b238aa2495277415d34538155a30b1b1d8e
Gerald W. Carter <gcarter@likewiseopen.org>
2011-01-18 04:43:49 +0000

    MERGE: -c 52912 /trunk/Platform -> /branches/Platform-CIFS-6.0
    13425: SIZEOF_MISMATCH
    Use same sizeof() expression in original allocate in realloc.
    
    (lwreg: r54419)

--- lwreg/shell/cmdparse.c
+++ lwreg/shell/cmdparse.c
@@ -1947,7 +1947,7 @@ RegShellCmdlineParseToArgv(
                             dwError = RegReallocMemory(
                                           pszArgv,
                                           (LW_PVOID) &pszArgvRealloc,
-                                          dwAllocSize * sizeof(PSTR *));
+                                          dwAllocSize * sizeof(*pszArgv));
                             BAIL_ON_REG_ERROR(dwError);
                             pszArgv = pszArgvRealloc;
                             pszArgvRealloc = NULL;
