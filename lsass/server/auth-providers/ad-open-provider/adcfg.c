c7034b8c60e2b3a61fcb4dc0e0b18f608096b3cc
Gerald W. Carter <gcarter@likewiseopen.org>
2011-01-18 05:05:55 +0000

    Revert merge of 52482 from /trunk/Platform
    MERGE: -c 52482 /trunk/Platform -> /branches/Platform-CIFS-6.0
    
    Fix memory leak in lsass.
    
    CR:  abernstein
    
    (lsass: r54441)

--- lsass/server/auth-providers/ad-open-provider/adcfg.c
+++ lsass/server/auth-providers/ad-open-provider/adcfg.c
@@ -504,8 +504,6 @@ AD_ReadRegistry(
 cleanup:
     LW_SAFE_FREE_STRING(pszUmask);
     LW_SAFE_FREE_STRING(pszUnresolvedMemberList);
-    LW_SAFE_FREE_STRING(pszExcludeTrustsListMultiString);
-    LW_SAFE_FREE_STRING(pszIncludeTrustsListMultiString);
 
     AD_FreeConfigContents(&StagingConfig);
 
