3021404af9e878bb1631a889e0b32e43383a7336
Scott Salley <ssalley@likewise.com>
2011-02-04 19:00:20 +0000

    Merge: -c 55319 ^/trunk/Platform lwupgrade: Fix bug 12129 -- null pointer dereference.
    (lwconfig: r55320)

--- lwconfig/lwconfig/src/capability.c
+++ lwconfig/lwconfig/src/capability.c
@@ -1633,7 +1633,8 @@ CapabilityShow(
     else
     {
         fprintf(stdout, "Name: %s\n", pCapability->pszName);
-        fprintf(stdout, "Description: %s\n", pCapability->pszDescription);
+        fprintf(stdout, "Description: %s\n",
+                pCapability->pszDescription ? pCapability->pszDescription : "");
         if (!bRegistryValueFound)
         {
             fprintf(stdout, "Type (Missing From Registry): %s\n",
