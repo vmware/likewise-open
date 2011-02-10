b449479b0ad1cdb6f6589fa6e048c00c98b9e4a2
Gerald W. Carter <gcarter@likewiseopen.org>
2011-02-10 21:58:02 +0000

    MERGE: -c 55556 /trunk/Platform -> /branches/Platform-CIFS-6.0
    lsass: BUG 12135 - Ignore failures enumerating trusts other than our primary domain
    
    Fixes domain join failures due to bailing in AD_Activate when forest trust
    numeration fails.  Adds better logging including non-sqashed error code
    for failures.
    
    CR: dalmeida@likewise.com
    
    (lsass: r55558)

--- lsass/server/auth-providers/ad-open-provider/adnetapi.c
+++ lsass/server/auth-providers/ad-open-provider/adnetapi.c
@@ -1175,14 +1175,7 @@ AD_DsEnumerateDomainTrusts(
         }
         else
         {
-            switch (winError)
-            {
-            case ERROR_ACCESS_DENIED:
-                dwError = winError;
-                break;
-            default:
-                dwError = LW_ERROR_ENUM_DOMAIN_TRUSTS_FAILED;
-            }
+            dwError = winError;
 
             if (AD_WinErrorIsConnectionError(winError))
             {
