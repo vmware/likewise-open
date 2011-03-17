843c995fda9cd016e0ca3e30f275b9a0d37648b0
Rafal Szczesniak <rafal@likewise.com>
2011-03-17 13:44:24 +0000

    srvsvc: Remove unused macros.
    (srvsvc: r57357)

--- srvsvc/server/defs.h
+++ srvsvc/server/defs.h
@@ -5,18 +5,6 @@
 
 #define SRV_DRIVER_NAME_W { '\\', 's', 'r', 'v', 0 }
 
-#define SRV_IOCTL_ADD_SHARE 0
-#define SRV_IOCTL_SET_SHARE 1
-
-#define ERROR_NOT_SUPPORTED 50
-
-/* Work around lack of proper Win32 error codes for now */
-
-#define WIN32_ERROR_SUCCESS                     0x00000000
-#define WIN32_ERROR_INVALID_FUNCTION            0x00000001
-#define WIN32_ERROR_FILE_NOT_FOUND              0x00000002
-#define WIN32_ERROR_UNKNOWN_LEVEL               0x0000007c
-
 #define SECURITY_UNMAPPED_UNIX_AUTHORITY    { 0, 0, 0, 0, 0, 22 }
 #define SECURITY_UNMAPPED_UNIX_UID_RID      1
 #define SECURITY_UNMAPPED_UNIX_RID_COUNT    2
