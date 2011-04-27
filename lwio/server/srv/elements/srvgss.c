7ef29e750eec0600776b87e4ed9406212ad81124
Gerald W. Carter <gcarter@likewiseopen.org>
2011-04-27 15:55:02 +0000

    MERGE: -c 58388 /trunk/Platform -> /branches/Platform-OEM-6.1
    srv: BUG 12500 - Remove unnecessary SrvGssNegotiate() state
    
    Fix regression on standalone servers introduced in SVN r50986.
    The krb5 make_NegHonts9) call was failing due to an error from getaddrinfo().
    A badly configured hostname that would not resolve would result in a failure
    to generate the mechList hints blob.  The SMB/SMB2 negotiate code deals
    with this as a non-fatal error however, the session setup code was not.
    
    The fix is to initiate the SrvGSS state at the negotiate stage and avoid
    the unnecessary NEG_HINTs call.
    
    (lwio: r58675)

--- lwio/server/srv/elements/srvgss.c
+++ lwio/server/srv/elements/srvgss.c
@@ -302,7 +302,7 @@ SrvGssBeginNegotiate(
                     (PVOID*)&pGssNegotiate);
     BAIL_ON_NT_STATUS(ntStatus);
 
-    pGssNegotiate->state = SRV_GSS_CONTEXT_STATE_INITIAL;
+    pGssNegotiate->state = SRV_GSS_CONTEXT_STATE_NEGOTIATE;
 
     pGssNegotiate->GssContext = GSS_C_NO_CONTEXT;
 
@@ -340,16 +340,6 @@ SrvGssNegotiate(
 
     switch(pGssNegotiate->state)
     {
-        case SRV_GSS_CONTEXT_STATE_INITIAL:
-
-            if (pSecurityInputBlob)
-            {
-                ntStatus = STATUS_INVALID_PARAMETER_3;
-                BAIL_ON_NT_STATUS(ntStatus);
-            }
-
-            // intentional fall through
-
         case SRV_GSS_CONTEXT_STATE_HINTS:
 
             ntStatus = SrvGssContinueNegotiate(
@@ -358,6 +348,16 @@ SrvGssNegotiate(
                             0,
                             &pSecurityBlob,
                             &ulSecurityBlobLen);
+
+            break;
+
+        case SRV_GSS_CONTEXT_STATE_INITIAL:
+
+            if (pSecurityInputBlob)
+            {
+                ntStatus = STATUS_INVALID_PARAMETER_3;
+                BAIL_ON_NT_STATUS(ntStatus);
+            }
 
             break;
 
