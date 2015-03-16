8de65e60a9edd7e66e6aa04f115d02b453a3f4bb
root <root@sndiscus.eng.vmware.com>
2015-03-16 20:36:49 +0000

    Change prefix to /opt/likewise

--- release/discusbuild64.sh
+++ release/discusbuild64.sh
@@ -9,9 +9,9 @@ export LW_DEVICE_PROFILE="discus"
 
 export CFLAGS="-Wno-error=unused-but-set-variable -Wno-error=implicit-function-declaration -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-local-typedefs -Wno-error=pointer-sign -Wno-error=address"
 ../configure $DEBUG \
-             --prefix=/usr \
-             --datadir=/usr/share \
-             --datarootdir=/usr/share \
+             --prefix=/opt/likewise \
+             --datadir=/opt/likewise/share \
+             --datarootdir=/opt/likewise/share \
              --build-isas=x86_64 \
              --lw-bundled-libs='libedit'
 
