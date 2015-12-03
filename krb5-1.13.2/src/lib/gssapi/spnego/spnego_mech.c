f81232e7bc61e10ec92143e99661774ab7dbc1f8
Adam Bernstein <abernstein@vmware.com>
2015-12-02 16:26:34 -0800

    Fix memory leak in GSSAPI/SPNEGO during failed authentication (2)
    Fix invalid memory reference introduced during refactor.
    
    Note: This change reverts this file back to its original state. The
    only addition is lines 1072-1075.
    
    Implemented ssalley review comment about redundant test on line 1072; 
    context_handle is known to be a valid pointer by that line of code.
    
    QA Notes: 
    Testing Done: ESX SB build: https://buildweb.eng.vmware.com/sb/6441033/; verified domainjoin-cli works
      Local build; valgrind echo_client to verify no memory leak is present
    Documentation Notes: 
    Bug Number: 1514618
    Reviewed by: ssalley
    Approved by: krishnag
    Mailto: vmidentity-checkins
    # Set "Merge to" values below to YES, SVS, NO, or MANUAL.
    # If you're unsure, ask your manager or your peers, DO NOT guess.
    Merge to: main: YES ## status=success; info=72880;
    
    Copied from Perforce
     Change: 72879

--- krb5-1.13.2/src/lib/gssapi/spnego/spnego_mech.c
+++ krb5-1.13.2/src/lib/gssapi/spnego/spnego_mech.c
@@ -979,7 +979,7 @@ spnego_gss_init_sec_context(
 	gss_buffer_t mechtok_in, mechListMIC_in, mechListMIC_out;
 	gss_buffer_desc mechtok_out = GSS_C_EMPTY_BUFFER;
 	spnego_gss_cred_id_t spcred = NULL;
-	spnego_gss_ctx_id_t spnego_ctx = (spnego_gss_ctx_id_t)*context_handle;
+	spnego_gss_ctx_id_t spnego_ctx = NULL;
 
 	dsyslog("Entering init_sec_context\n");
 
@@ -1042,6 +1042,7 @@ spnego_gss_init_sec_context(
 
 	/* Step 2: invoke the selected or optimistic mechanism's
 	 * gss_init_sec_context function, if it didn't complete previously. */
+	spnego_ctx = (spnego_gss_ctx_id_t)*context_handle;
 	if (!spnego_ctx->mech_complete) {
 		ret = init_ctx_call_init(
 			minor_status, spnego_ctx, spcred,
@@ -1068,6 +1069,10 @@ spnego_gss_init_sec_context(
 				 &negState, &send_token);
 	}
 cleanup:
+        if (!spnego_ctx) {
+	    spnego_ctx = (spnego_gss_ctx_id_t)*context_handle;
+        }
+
 	if (send_token == INIT_TOKEN_SEND) {
 		if (make_spnego_tokenInit_msg(spnego_ctx,
 					      0,
