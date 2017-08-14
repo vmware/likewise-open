vmware/likewise-open

commit 81d013202655fe3e5f7cbce2ffca2e8cbdef42ef

Remove 2048 character restriction on strings and multi strings
Remove maximum data value length restriction. The Registry specifications
do not specify a maximum size for the data of an value entry. The existing
maximum is too small to be of much use for some data types (REG_MULTI_SZ).

Change-Id: I3db4c78c69f8a3a6fb63e311e04ea411816e5905

lwreg/shell/export.c
@@ -295,6 +295,7 @@ ProcessExportedKeyInfo(
     REG_DATA_TYPE dataType = REG_NONE;
     BYTE *value = NULL;
     DWORD dwValueLen = 0;
+    DWORD dwValueLenMax = 0;
     int iCount = 0;
     DWORD dwValuesCount = 0;
     PVOID pValue = NULL;
@@ -305,9 +306,6 @@ ProcessExportedKeyInfo(
     REG_PARSE_ITEM regItem = {0};
     FILE *fp = pExportState->fp;
     BOOLEAN bValueSet = FALSE;
-
-    dwError = RegAllocateMemory(MAX_VALUE_LENGTH * 2, (PVOID*)&value);
-    BAIL_ON_REG_ERROR(dwError);
 
     dwError = PrintToRegFile(
                           fp,
@@ -333,7 +331,7 @@ ProcessExportedKeyInfo(
         NULL,
         &dwValuesCount,
         NULL,
-        NULL,
+        &dwValueLenMax,
         NULL,
         NULL);
     BAIL_ON_REG_ERROR(dwError);
@@ -343,12 +341,15 @@ ProcessExportedKeyInfo(
         goto cleanup;
     }
 
+    dwError = RegAllocateMemory(dwValueLenMax, (PVOID*)&value);
+    BAIL_ON_REG_ERROR(dwError);
+
     for (iCount = 0; iCount < dwValuesCount; iCount++)
     {
         memset(pwszValueName, 0, MAX_KEY_LENGTH);
         dwValueNameLen = MAX_KEY_LENGTH;
-        memset(value, 0, MAX_VALUE_LENGTH * 2);
-        dwValueLen = MAX_VALUE_LENGTH;
+        memset(value, 0, dwValueLenMax);
+        dwValueLen = dwValueLenMax;
         RegSafeFreeCurrentValueInfo(&pCurrValueInfo);
 
         dwError = RegEnumValueW((HANDLE)hReg,
@@ -537,8 +538,10 @@ cleanup:
     LWREG_SAFE_FREE_STRING(pszValue);
     LWREG_SAFE_FREE_STRING(regItem.regAttr.pDefaultValue);
     RegSafeFreeCurrentValueInfo(&pCurrValueInfo);
-    memset(value, 0 , MAX_KEY_LENGTH);
     LWREG_SAFE_FREE_MEMORY(value);
+
+    if (dwValueLenMax)
+       memset(value, 0 , dwValueLenMax);
 
     return dwError;
 

lwreg/client/regntclient.c
@@ -587,11 +587,6 @@ NtRegEnumValueA(
     	status = RtlSafeMultiplyULONG(&cTempData, *pcbData, sizeof(WCHAR));
     	BAIL_ON_NT_STATUS(status);
 
-    	if (cTempData > MAX_VALUE_LENGTH)
-    	{
-    		cTempData = MAX_VALUE_LENGTH;
-    	}
-
     	if (cTempData)
     	{
             status = LW_RTL_ALLOCATE(&pTempData, VOID, cTempData);
@@ -780,13 +775,8 @@ NtRegGetValueA(
     {
     	cTempData = (*pcbData)*sizeof(WCHAR);
 
-    	if (cTempData > MAX_VALUE_LENGTH)
-    	{
-    		cTempData = MAX_VALUE_LENGTH;
-    	}
-
-	    status = LW_RTL_ALLOCATE(&pTempData, VOID, cTempData*sizeof(WCHAR));
-	    BAIL_ON_NT_STATUS(status);
+	status = LW_RTL_ALLOCATE(&pTempData, VOID, cTempData*sizeof(WCHAR));
+	BAIL_ON_NT_STATUS(status);
     }
 
 	status = RegTransactGetValueW(

lwreg/server/providers/sqlite/regschema.c
@@ -100,12 +100,6 @@ SqliteSetValueAttributes(
     pKeyCtxInUse = pKeyHandleInUse->pKey;
     BAIL_ON_INVALID_KEY_CONTEXT(pKeyCtxInUse);
 
-    if (MAX_VALUE_LENGTH < pValueAttributes->DefaultValueLen)
-    {
-        status = STATUS_INVALID_BLOCK_LENGTH;
-        BAIL_ON_NT_STATUS(status);
-    }
-
     status = LwRtlWC16StringDuplicate(&pwszValueName, !pValueName ? wszEmptyValueName : pValueName);
     BAIL_ON_NT_STATUS(status);
 

lwreg/server/providers/sqlite/sqliteapi.c
@@ -764,12 +764,6 @@ SqliteSetValueEx(
     pKeyCtx = pKeyHandle->pKey;
     BAIL_ON_INVALID_KEY_CONTEXT(pKeyCtx);
 
-    if (MAX_VALUE_LENGTH < cbData)
-    {
-    	status = STATUS_INVALID_BLOCK_LENGTH;
-    	BAIL_ON_NT_STATUS(status);
-    }
-
     status = LwRtlWC16StringDuplicate(&pwszValueName, !pValueName ? wszEmptyValueName : pValueName);
     BAIL_ON_NT_STATUS(status);
 

lwsm/server/registry.c
@@ -357,12 +357,9 @@ LwSmRegistryReadString(
     )
 {
     DWORD dwError = 0;
-    WCHAR wszValue[MAX_VALUE_LENGTH];
     DWORD dwSize = 0;
     DWORD dwType = 0;
-
-    memset(wszValue, 0, sizeof(wszValue));
-    dwSize = sizeof(wszValue);
+    PWSTR pwszValue = NULL;
 
     dwError = RegGetValueW(
         hReg,
@@ -371,12 +368,25 @@ LwSmRegistryReadString(
         pwszValueName,
         RRF_RT_REG_SZ,
         &dwType,
-        wszValue,
+        pwszValue,
         &dwSize);
     BAIL_ON_ERROR(dwError);
 
-    dwError = LwAllocateWc16String(ppwszValue, wszValue);
+    dwError = LwAllocateMemory(dwSize + sizeof(WCHAR), OUT_PPVOID(&pwszValue));
     BAIL_ON_ERROR(dwError);
+
+    dwError = RegGetValueW(
+        hReg,
+        pRootKey,
+        pwszParentKey,
+        pwszValueName,
+        RRF_RT_REG_SZ,
+        &dwType,
+        pwszValue,
+        &dwSize);
+    BAIL_ON_ERROR(dwError);
+
+    *ppwszValue = pwszValue;
 
 cleanup:
 

commit 8ba8529761d369390f5379d3dc2bb6f2b49e1cca

Bug fixes: lwregshell and lsass samr
samr_crypto.c:SamrSrvDecryptPasswordBlobEx() was not properly constructing
the RC4 key to decrypt the password blob. As a result, the password blob
decryption failed. The maximum number of bytes allowed is 16, according o
to MS-SAMR.pdf in section 3.2.2.2 MD5 Usage.  This change matches
the key construction performed in the lsassd ad-open-provider/join/join.c code.

lwregshell export.c@81d013202655fe3e5f7cbce2ffca2e8cbdef42ef broke
"/opt/likewise/bin/lwregshell export". This change resulted in a SIGSEGV
when memset() after free'ing the temporary "value" variable.

Enabled FIPS mode in echo_server.
Change-Id: Ic7754eb2db0c0b0e415d384d8fa9a2321c78e226

lsass/server/rpc/samr/samr_crypto.c
@@ -128,11 +128,14 @@ SamrSrvDecryptPasswordBlobEx(
            sizeof(KeyInit));
 
     /*
-     * Prepare the session key digested with key initialisator
+     * Prepare the session key digested with key initializer.
+     *
+     * Session key should be 16 bytes according to Microsoft's MS-SAMR.pdf
+     * in section 3.2.2.2 MD5 Usage. RC4 happens to have a 16 byte session key.
      */
     MD5_Init(&ctx);
     MD5_Update(&ctx, KeyInit, sizeof(KeyInit));
-    MD5_Update(&ctx, pKey, dwKeyLen);
+    MD5_Update(&ctx, pKey, dwKeyLen > 16 ? 16 : dwKeyLen);
     MD5_Final(DigestedSessionKey, &ctx);
 
     /*
@@ -452,8 +455,8 @@ SamrSrvEncryptPasswordBlobEx(
      * Prepare the session key digested with key initialisator
      */
     MD5_Init(&ctx);
-    MD5_Update(&ctx, pKeyInit, dwKeyInitLen);
-    MD5_Update(&ctx, pKey, dwKeyLen);
+    MD5_Update(&ctx, pKeyInit, dwKeyInitLen > 16 ? 16 : dwKeyInitLen);
+    MD5_Update(&ctx, pKey, dwKeyLen > 16 ? 16 : dwKeyLen);
     MD5_Final(DigestedSessionKey, &ctx);
 
     /*

lwreg/shell/export.c
@@ -538,10 +538,10 @@ cleanup:
     LWREG_SAFE_FREE_STRING(pszValue);
     LWREG_SAFE_FREE_STRING(regItem.regAttr.pDefaultValue);
     RegSafeFreeCurrentValueInfo(&pCurrValueInfo);
-    LWREG_SAFE_FREE_MEMORY(value);
+    if (dwValueLenMax && value)
+       memset(value, 0, dwValueLenMax);
 
-    if (dwValueLenMax)
-       memset(value, 0 , dwValueLenMax);
+    LWREG_SAFE_FREE_MEMORY(value);
 
     return dwError;
 