a95ea001f894c618b2ed375206e6bd0e771d28b1
Adam Bernstein <abernstein@vmware.com>
2017-12-15 19:59:53 +0000

    Trivial change to make.sh
    Allow command line override to default build target, which is
    all package. This allows make.sh clean, for example.
    
    Change-Id: I053a12d23d4968496d3dabc388193e70f1a7e39f

--- release/make.sh
+++ release/make.sh
@@ -2,6 +2,12 @@
 
 umask 0022
 
+if [ -n "$1" ]; then
+  target="$1"
+  shift
+else
+  target="all package"
+fi
 export CWD=`pwd`
 
 export LW_FEATURE_LEVEL="auth"
@@ -9,5 +15,4 @@ export LSA_RPC_SERVERS="yes"
 export LW_DEVICE_PROFILE="photon"
 
 export CFLAGS="-g -O0 -Wno-error=unused-but-set-variable -Wno-error=implicit-function-declaration -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-local-typedefs -Wno-error=pointer-sign -Wno-error=address"
-make all package
-
+make $target
