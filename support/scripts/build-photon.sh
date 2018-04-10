68c7ecf2b329d73046e8a9451c75d0c8132f8e68
Sriram Nambakam <snambakam@vmware.com>
2018-04-09 18:17:43 -0700

    Add convenience script to help install packages needed to build on Photon
    Add Hypermake to help build likewise-open on Photon OS 2.0 docker toolchain image
    
    Change-Id: I0653e818d02fa26e08e83528a6a6db1d88810b62

---
+++ support/scripts/build-photon.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DEBUG=0
+
+while getopts d: o
+do
+    case "$o" in
+        d)
+            DEBUG="$OPTARG"
+            ;;
+        [?])
+            showUsage
+            exit 1
+    esac
+done
+
+PROJECT_ROOT=$(pwd)
+
+if [ "$DEBUG" = "1" ]; then
+    DEBUG=1 mkdir -p $PROJECT_ROOT/debug && \
+    cd $PROJECT_ROOT/debug && \
+        ../release/photonbuild64.sh
+else
+    cd $PROJECT_ROOT/release && \
+        ./photonbuild64.sh
+fi
---
+++ support/scripts/photon2-toolchain-setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+tdnf update -qy  tdnf  && \
+tdnf install -y autoconf-2.69 \
+                automake-1.15 \
+                binutils-2.29.1 \
+                bison-3.0.4 \
+                e2fsprogs-devel-1.43.4 \
+                gawk-4.1.4 \
+                gcc-6.3.0 \
+                glibc-devel-2.26 \
+                krb5-1.16 \
+                krb5-devel-1.16 \
+                libtool-2.4.6 \
+                libxml2-2.9.7 \
+                libxml2-devel-2.9.7 \
+                linux-api-headers-4.9.74 \
+                Linux-PAM-1.3.0 \
+                Linux-PAM-devel-1.3.0 \
+                make-4.2.1 \
+                ncurses-6.0 \
+                ncurses-devel-6.0 \
+                openldap-2.4.44 \
+                openldap-devel-2.4.44 \
+                openssl-1.0.2n \
+                openssl-devel-1.0.2n \
+                popt-1.16 \
+                popt-devel-1.16 \
+                procps-ng-3.3.12 \
+                rpm-4.13.0.1 \
+                rpm-build-4.13.0.1 \
+                rpm-devel-4.13.0.1 \
+                sed-4.4 \
+                shadow-4.2.1 \
+                sqlite-3.22.0 \
+                sqlite-devel-3.22.0 \
+                tar-1.29 \
+                util-linux-devel-2.29.2 \
+                zlib-1.2.11 \
+                zlib-devel-1.2.11
+
+
